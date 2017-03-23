79e849c98797b0ef8381ad2316baca365ff87fef
yuhansun <wdmzssyh@gmail.com>
2017-03-22 19:04:31 -0700

    ini

---
+++ src/main/java/commons/Query_Graph.java
@@ -0,0 +1,37 @@
+package commons;
+
+import java.awt.geom.GeneralPath;
+import java.util.ArrayList;
+
+public class Query_Graph {
+	public int[] label_list;
+	ArrayList<ArrayList<Integer>> graph;
+	public MyRectangle[] spa_predicate;
+	public boolean[] Has_Spa_Predicate;
+	
+	public Query_Graph(int node_count) {
+		label_list = new int[node_count];
+		graph = new ArrayList<ArrayList<Integer>>(node_count);
+		for ( int i = 0; i < node_count; i++)
+			graph.add(new ArrayList<Integer>());
+		spa_predicate = new MyRectangle[node_count];
+		Has_Spa_Predicate = new boolean[node_count];
+	}
+	
+	@Override
+	public String toString()
+	{
+		String string = "";
+		for ( int i = 0; i < label_list.length; i++)
+		{
+			ArrayList<Integer> neighbors = graph.get(i);
+			string += String.format("%d,%d", i, neighbors.size());
+			for (int neighbor : neighbors)
+				string += String.format(",%d", neighbor);
+			string += "," + String.valueOf(Has_Spa_Predicate[i]);
+			string += String.format(",%d\n", label_list[i]);
+		}
+		
+		return string;
+	}
+}
---
+++ src/main/java/graph/Construct_RisoTree.java
@@ -0,0 +1,17 @@
+package graph;
+
+public class Construct_RisoTree {
+
+	static int max_hop = 2;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void ConstructSize()
+	{
+		
+	}
+	
+}
