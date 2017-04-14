f5790bac304f965c15efe8b1db850015c91eb685
Yuhan Sun <wdmzssyh@gmail.com>
2017-04-13 23:32:10 -0700

    ignore (reverted from commit 302d4146a924ec14b0e4330febf1539f43ff013f)

--- src/main/java/graph/RisoTreeQuery.java
+++ src/main/java/graph/RisoTreeQuery.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.TreeSet;
 
 import org.neo4j.gis.spatial.rtree.RTreeRelationshipTypes;
 import org.neo4j.graphdb.Direction;
@@ -16,7 +15,6 @@ import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-
 import commons.Config;
 import commons.MyRectangle;
 import commons.OwnMethods;
@@ -81,7 +79,7 @@ public class RisoTreeQuery {
 		return minhop_index;
 	}
 	
-	public static TreeSet<Long> Query(Query_Graph query_Graph, Node root_node, int max_hopnum)
+	public static void Query(Query_Graph query_Graph, Node root_node, int max_hopnum)
 	{
 		try {
 			//<spa_id, rectangle>
@@ -89,10 +87,8 @@ public class RisoTreeQuery {
 			
 			//<spa_id, <neighbor_id, hop_num>>
 			HashMap<Integer, HashMap<Integer, Integer>> min_hop = new HashMap<>();
-			//<spa_id, <neighbor_id, size_property_name>>
+			//<spa_id, <neighbor_id, property_name>>
 			HashMap<Integer, HashMap<Integer, String>> NL_size_propertyname = new HashMap<>();
-			//<spa_id, <neighbor_id, list_property_name>>
-			HashMap<Integer, HashMap<Integer, String>> NL_list_propertyname = new HashMap<Integer, HashMap<Integer,String>>();	
 			//<spa_id, <neighbor_id, NL_list>>
 			HashMap<Integer, HashMap<Integer, HashSet<Integer>>> NL_list = new HashMap<>();
 			
@@ -102,9 +98,8 @@ public class RisoTreeQuery {
 				if(query_Graph.Has_Spa_Predicate[i])
 				{
 					spa_predicates.put(i, query_Graph.spa_predicate[i]);
-					min_hop.put(i, new HashMap<Integer, Integer>());
-					NL_size_propertyname.put(i, new HashMap<Integer, String>());
-					NL_list_propertyname.put(i, new HashMap<Integer, String>());
+					min_hop.put(i, new HashMap<>());
+					NL_size_propertyname.put(i, new HashMap<>());
 					for ( int j = 0; j < min_hop_array[i].length; j++)
 					{
 						if(min_hop_array[i][j] <= max_hopnum && min_hop_array[i][j] != -1)
@@ -112,7 +107,6 @@ public class RisoTreeQuery {
 							min_hop.get(i).put(j, min_hop_array[i][j]);
 							int label = query_Graph.label_list[j];
 							NL_size_propertyname.get(i).put(j, String.format("NL_%d_%d_size", min_hop_array[i][j], label));
-							NL_list_propertyname.get(i).put(j, String.format("NL_%d_%d_list", min_hop_array[i][j], label));
 						}
 					}
 				}
@@ -127,7 +121,6 @@ public class RisoTreeQuery {
 			int level_index = 0;
 			while(cur_list.isEmpty() == false)
 			{
-				long start = System.currentTimeMillis();
 				//<spa_id, card>
 				HashMap<Integer, Double> spa_cards = new HashMap<>();
 				for (int key : spa_predicates.keySet())
@@ -137,7 +130,7 @@ public class RisoTreeQuery {
 				HashMap<Integer, HashMap<Integer, Double>> NL_cards = new HashMap<>();
 				for (int spa_id : min_hop.keySet())
 				{
-					NL_cards.put(spa_id, new HashMap<Integer, Double>());
+					NL_cards.put(spa_id, new HashMap<>());
 					HashMap<Integer, Integer> min_hop_vector = min_hop.get(spa_id);
 					for ( int neighbor_pos : min_hop_vector.keySet())
 						NL_cards.get(spa_id).put(neighbor_pos, 0.0);	
@@ -190,7 +183,9 @@ public class RisoTreeQuery {
 						throw new Exception(String.format("node %d does not has \"bbox\" property", node));
 				}
 				
+				
 				OwnMethods.Print(String.format("level %d", level_index));
+				level_index++;
 				
 				double min_spa_card = Double.MAX_VALUE, min_NL_card = Double.MAX_VALUE;
 				int min_NL_spa_id = 0, min_NL_neighbor_id = 0;
@@ -221,63 +216,21 @@ public class RisoTreeQuery {
 				if (overlap_MBR_list.isEmpty() == true)
 				{
 					OwnMethods.Print("No result satisfy the query.");
-					return null;
+					return;
 				}
 				
-				long start1 = System.currentTimeMillis();
-//				if ( min_NL_card < min_spa_card)
-//				{
-//					String property_name = NL_list_propertyname.get(min_NL_spa_id).get(min_NL_neighbor_id);
-//					
-//					HashSet<Integer> min_NL_list = new HashSet<Integer>();
-////					NL_list.put(min_NL_spa_id, new HashMap<Integer, HashSet<Integer>>());
-////					NL_list.get(min_NL_spa_id).put(min_NL_neighbor_id, new HashSet<Integer>());
-//					for ( Node node : overlap_MBR_list)
-//					{
-//						int[] NL_list_label = ( int[] ) node.getProperty(property_name);
-//						for ( int node_id : NL_list_label)
-//							min_NL_list.add(node_id);
-//					}
-//					OwnMethods.Print(String.format("min_NL_list size is %d", min_NL_list.size()));
-//					if ( min_NL_list.size() < min_spa_card)
-//					{
-//						OwnMethods.Print("NL_list is smaller");
-//					}
-//				}
-//				OwnMethods.Print(String.format("NL_serialize time: %d", System.currentTimeMillis() - start1));
-				
-				OwnMethods.Print(String.format("level %d time: %d", level_index, System.currentTimeMillis() - start));
-				
-				int located_in_count = 0;
-				if( overlap_MBR_list.isEmpty() == false && next_list.isEmpty())
+				if ( min_NL_card < min_spa_card)
 				{
-					TreeSet<Long> ids = new TreeSet<Long>(); 
-					start = System.currentTimeMillis();
+//					String property_name = NL_
 					for ( Node node : overlap_MBR_list)
-						for ( Relationship relationship : node.getRelationships(
-								Direction.OUTGOING, RTreeRelationshipTypes.RTREE_REFERENCE))
-						{
-							Node geom = relationship.getEndNode();
-							double[] bbox = (double[]) geom.getProperty("bbox");
-							MyRectangle rectangle = new MyRectangle(bbox);
-							for ( int key : spa_predicates.keySet())
-								if ( rectangle.intersect(spa_predicates.get(key)) != null)
-								{
-									ids.add(geom.getId());
-									located_in_count++;
-								}
-						}
-					OwnMethods.Print(String.format("Located in nodes: %d", located_in_count));
-					level_index++;
-					OwnMethods.Print(String.format("level %d time: %d", level_index, System.currentTimeMillis() - start));
-					return ids;
+					{
+						
+					}
 				}
 				
 				cur_list = next_list;
 				next_list = new LinkedList<>();
 				
-
-				level_index++;
 				
 				
 			}
@@ -286,72 +239,35 @@ public class RisoTreeQuery {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
-		return null;
 	}
 	
 	static String dataset = "Gowalla";
 	static Config config = new Config();
 	static String version = config.GetNeo4jVersion();
-//	static String db_path = String.format("/home/yuhansun/Documents/GeoGraphMatchData/%s_%s/data/databases/graph.db", version, dataset);
-//	static String querygraph_path = "/mnt/hgfs/Ubuntu_shared/GeoMinHop/query/query_graph.txt";
-	static String db_path = "D:\\Ubuntu_shared\\GeoMinHop\\data\\Gowalla\\neo4j-community-3.1.1_Gowalla\\data\\databases\\graph.db";
-	static String querygraph_path = "D:\\Ubuntu_shared\\GeoMinHop\\query\\query_graph.txt";
+	static String db_path = String.format("/home/yuhansun/Documents/GeoGraphMatchData/%s_%s/data/databases/graph.db", version, dataset);
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int query_id = 5;
+		String querygraph_path = "/mnt/hgfs/Ubuntu_shared/GeoMinHop/query/query_graph.txt";
 		ArrayList<Query_Graph> queryGraphs = Utility.ReadQueryGraph_Spa(querygraph_path, query_id + 1);
 		Query_Graph query_Graph = queryGraphs.get(query_id);
-//		query_Graph.spa_predicate[1] = new MyRectangle(-84.468680, 33.879658, -84.428434, 33.919904);
-		query_Graph.spa_predicate[1] = new MyRectangle(-98.025157, 29.953977, -97.641747, 30.337387);
-//		query_Graph.spa_predicate[1] = new MyRectangle(-91.713778, 14.589395, -68.517838, 37.785335);
+		query_Graph.spa_predicate[1] = new MyRectangle(-84.468680, 33.879658, -84.428434, 33.919904);
 //		query_Graph.spa_predicate[3] = new MyRectangle(-98.025157, 29.953977, -97.641747, 30.337387);
 		
 		try {
+			String db_path = String.format("/home/yuhansun/Documents/GeoGraphMatchData/%s_%s/data/databases/graph.db", version, dataset);
 			GraphDatabaseService dbservice = new GraphDatabaseFactory().newEmbeddedDatabase(new File(db_path));
 			
 			long start = System.currentTimeMillis();
 			Transaction tx = dbservice.beginTx();
 			Node root_node =  OSM_Utility.getRTreeRoot(dbservice, dataset);
-			TreeSet<Long> result1 = Query(query_Graph, root_node, 2);
+			Query(query_Graph, root_node, 2);
 			tx.success();
 			tx.close();
 			OwnMethods.Print("Time:" + (System.currentTimeMillis() - start));
 			dbservice.shutdown();
-			
-//			SpatialFirst spatialFirst = new SpatialFirst(db_path, dataset);
-//			TreeSet<Long> result2 = spatialFirst.Query(query_Graph);
-//			spatialFirst.shutdown();
-//			
-//			LinkedList<Long> counter1 = new LinkedList<Long>();
-//			for ( long id : result1)
-//				if(result2.contains(id) == false)
-//					counter1.add(id);
-//			
-//			for ( long id : counter1)
-//				OwnMethods.Print(id);
-//			OwnMethods.Print(counter1.size());
-			
-//			LinkedList<Long> counter2 = new LinkedList<Long>();
-//			for ( long id : result2)
-//				if(result1.contains(id) == false)
-//					counter2.add(id);
-//			
-//			for ( long id : counter2)
-//				OwnMethods.Print(id);
-//			OwnMethods.Print(counter2.size());
-			
-//			dbservice = new GraphDatabaseFactory().newEmbeddedDatabase(new File(db_path));
-//			tx = dbservice.beginTx();
-//			for ( long id : counter1)
-//			{
-//				double[] bbox = (double[])dbservice.getNodeById(id).getProperty("bbox");
-//				MyRectangle rectangle = new MyRectangle(bbox);
-////				for ( double element : bbox)
-////					OwnMethods.Print(element);
-//				OwnMethods.Print(rectangle.toString());
-////				break;
-//			}
 			
 		} catch (Exception e) {
 			// TODO: handle exception
