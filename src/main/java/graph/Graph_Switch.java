433909942fe34ec8054d646892b3280a0805b64e
yuhansun <wdmzssyh@gmail.com>
2017-06-21 13:47:07 -0700

    transfer labels, properties, edges respectively.

--- src/main/java/graph/Graph_Switch.java
+++ src/main/java/graph/Graph_Switch.java
@@ -3,18 +3,32 @@ package graph;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import javax.management.relation.Relation;
+import javax.sound.sampled.LineUnavailableException;
 
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.ResourceIterator;
+import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.unsafe.batchinsert.BatchInserter;
+import org.neo4j.unsafe.batchinsert.BatchInserters;
 
 import commons.Config;
 import commons.Labels;
 import commons.OwnMethods;
 import commons.Labels.GraphLabel;
+import commons.Labels.GraphRel;
+import commons.Labels.OSMLabel;
 import commons.Labels.OSMRelation;
 import commons.Config.system;
 import osm.OSM_Utility;
@@ -39,6 +53,7 @@ public class Graph_Switch {
 	static String graphLinkLabelName = Labels.GraphRel.GRAPH_LINK.name();
 	
 	static String db_path;
+	static String label_list_path;
 	
 	static int max_hop_num = 2;
 	
@@ -47,9 +62,11 @@ public class Graph_Switch {
 		switch (systemName) {
 		case Ubuntu:
 			db_path = String.format("/home/yuhansun/Documents/GeoGraphMatchData/%s_%s/data/databases/graph.db", version, dataset);
+			label_list_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/label.txt", dataset);
 			break;
 		case Windows:
 			db_path = String.format("D:\\Ubuntu_shared\\GeoMinHop\\data\\%s\\%s_%s\\data\\databases\\graph.db", dataset, version, dataset);
+			label_list_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/label.txt", dataset);
 		default:
 			break;
 		}
@@ -57,7 +74,259 @@ public class Graph_Switch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		initParameters();
-		transferGraph();
+//		transferProperty();
+		
+//		getNodesMapRTree();
+//		addLabel();
+//		removeLabel();
+//		removeNLLabelOnGraph_0();
+		
+//		labelTest();
+		
+//		deleteRelationship();
+		createNewRelationship();
+		
+//		transferLabel();
+//		addLabel();
+		
+	}
+	
+	public static void createNewRelationship()
+	{
+		BatchInserter inserter = null;
+		try {
+			String map_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/node_map_RTree.txt", dataset);
+			String graph_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/graph.txt", dataset);
+			
+			Map<String, String> id_map = OwnMethods.ReadMap(map_path);
+			ArrayList<ArrayList<Integer>> graph = OwnMethods.ReadGraph(graph_path);
+			
+			Map<String, String> config = new HashMap<String, String>();
+			config.put("dbms.pagecache.memory", "6g");
+			inserter = BatchInserters.inserter(new File(db_path).getAbsoluteFile(), config);
+			
+			for (int i = 0; i < graph.size(); i++)
+			{
+//				OwnMethods.Print(i);
+				ArrayList<Integer> neighbors = graph.get(i);
+				long start_neo4j_id = Long.parseLong(id_map.get(String.valueOf(i)));
+				for (int j = 0; j < neighbors.size(); j++)
+				{
+					int neighbor = neighbors.get(j);
+					if ( i < neighbor )
+					{
+						long end_neo4j_id = Long.parseLong(id_map.get(String.valueOf(neighbor)));
+						OwnMethods.Print(String.format("%d\t%d", start_neo4j_id, end_neo4j_id));
+						inserter.createRelationship(start_neo4j_id, end_neo4j_id, GraphRel.GRAPH_LINK, null);
+					}
+				}
+			}
+			inserter.shutdown();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			if ( inserter != null)
+				inserter.shutdown();
+			e.printStackTrace();	System.exit(-1);
+		}
+	}
+	
+	/**
+	 * remove edges from nonspatial to spatial vertices (osm nodes)
+	 */
+	public static void deleteRelationship()
+	{
+		try {
+			GraphDatabaseService dbservice = new GraphDatabaseFactory().newEmbeddedDatabase(new File(db_path));
+//			Transaction tx = dbservice.beginTx();
+//			Iterable<Node> leafNodes = OSM_Utility.getAllGeometries(dbservice, dataset);
+//			for ( Node leafNode : leafNodes)
+//			{
+//				Node osmNode = leafNode.getSingleRelationship(OSMRelation.GEOM, Direction.INCOMING)
+//						.getStartNode();
+//				
+//				//transfer relationship
+//				Iterable<Relationship> rels = osmNode.getRelationships(Direction.INCOMING, GraphRel.GRAPH_LINK);
+//				for ( Relationship relationship : rels)
+//					relationship.delete();
+//				
+//			}
+//			tx.success();
+//			tx.close();
+			dbservice.shutdown();
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	
+	public static void labelTest()
+	{
+		try {
+			GraphDatabaseService dbservice = new GraphDatabaseFactory().newEmbeddedDatabase(new File(db_path));
+			Transaction tx = dbservice.beginTx();
+			
+//			ResourceIterator<Node> nodes = dbservice.findNodes(GraphLabel.GRAPH_1); 
+//			Node node;
+//			while(nodes.hasNext())
+//			{
+//				node = nodes.next();
+//				OwnMethods.Print(node.getId());
+//				OwnMethods.Print(node.getAllProperties());
+//			}
+			
+			String query = "match (n:GRAPH_1) return n limit 10";
+			Result result = dbservice.execute(query);
+			while ( result.hasNext())
+			{
+				Map<String, Object> map = result.next();
+				Node node = (Node) map.get("n");
+				OwnMethods.Print(node.getAllProperties());
+			}
+			
+			tx.success();
+			tx.close();
+			dbservice.shutdown();
+		} catch (Exception e) {
+			e.printStackTrace();	System.exit(-1);
+		}
+	}
+	
+	/**
+	 * remove label of osm nodes, including 
+	 * on original GRAPH_0 nodes
+	 * original GRAPH_0 label is retained
+	 */
+	public static void removeNLLabelOnGraph_0()
+	{
+		BatchInserter inserter = null;
+		Map<String, String> config = new HashMap<String, String>();
+		config.put("dbms.pagecache.memory", "10g");
+		try {
+			
+			ArrayList<Integer> label_list = OwnMethods.readIntegerArray(label_list_path);
+			OwnMethods.Print(String.format("label_list size %d", label_list.size()));
+			
+			TreeSet<Integer> nonSpaIDs = new TreeSet<Integer>();
+			for ( int i = 0; i < label_list.size(); i++)
+				if(label_list.get(i).equals(1) == false)
+					nonSpaIDs.add(i);
+			OwnMethods.Print(String.format("spaIDs size %d", nonSpaIDs.size()));
+			
+			String map_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/node_map.txt", dataset);
+			Map<String, String> map_str = OwnMethods.ReadMap(map_path);
+			Map<Long, Long> map = new TreeMap<Long, Long>();
+			for ( String key : map_str.keySet())
+				map.put(Long.parseLong(key), Long.parseLong(map_str.get(key)));
+			OwnMethods.Print(String.format("map size %d", map.size()));
+			
+			inserter = BatchInserters.inserter(new File(db_path),config);
+			for ( int id : nonSpaIDs)
+			{
+				OwnMethods.Print(id);
+				long pos_id = map.get((long)id);
+				inserter.setNodeLabels(pos_id, GraphLabel.GRAPH_0);
+			}
+			inserter.shutdown();
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			if ( inserter != null)
+				inserter.shutdown();
+			System.exit(-1);
+		}
+	}
+	
+	/**
+	 * remove label of osm nodes, including 
+	 * on original GRAPH_1 nodes
+	 */
+	public static void removeLabel()
+	{
+		BatchInserter inserter = null;
+		Map<String, String> config = new HashMap<String, String>();
+		config.put("dbms.pagecache.memory", "10g");
+		try {
+			
+			ArrayList<Integer> label_list = OwnMethods.readIntegerArray(label_list_path);
+			OwnMethods.Print(String.format("label_list size %d", label_list.size()));
+			
+			TreeSet<Integer> spaIDs = new TreeSet<Integer>();
+			for ( int i = 0; i < label_list.size(); i++)
+				if(label_list.get(i).equals(1))
+					spaIDs.add(i);
+			OwnMethods.Print(String.format("spaIDs size %d", spaIDs.size()));
+			
+			String map_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/node_map.txt", dataset);
+			Map<String, String> map_str = OwnMethods.ReadMap(map_path);
+			Map<Long, Long> map = new TreeMap<Long, Long>();
+			for ( String key : map_str.keySet())
+				map.put(Long.parseLong(key), Long.parseLong(map_str.get(key)));
+			OwnMethods.Print(String.format("map size %d", map.size()));
+			
+			inserter = BatchInserters.inserter(new File(db_path),config);
+			for ( int id : spaIDs)
+			{
+				OwnMethods.Print(id);
+				long pos_id = map.get((long)id);
+				inserter.setNodeLabels(pos_id, OSMLabel.OSM_NODE);
+			}
+			inserter.shutdown();
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			if ( inserter != null)
+				inserter.shutdown();
+			System.exit(-1);
+		}
+	}
+	
+	/**
+	 * add label to R-tree leaf nodes
+	 */
+	public static void addLabel()
+	{
+		BatchInserter inserter = null;
+		Map<String, String> config = new HashMap<String, String>();
+		config.put("dbms.pagecache.memory", "10g");
+		try {
+			
+			ArrayList<Integer> label_list = OwnMethods.readIntegerArray(label_list_path);
+			OwnMethods.Print(String.format("label_list size %d", label_list.size()));
+			
+			TreeSet<Integer> spaIDs = new TreeSet<Integer>();
+			for ( int i = 0; i < label_list.size(); i++)
+				if(label_list.get(i).equals(1))
+					spaIDs.add(i);
+			OwnMethods.Print(String.format("spaIDs size %d", spaIDs.size()));
+			
+			String map_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/node_map_RTree.txt", dataset);
+			Map<String, String> map_str = OwnMethods.ReadMap(map_path);
+			Map<Long, Long> map = new TreeMap<Long, Long>();
+			for ( String key : map_str.keySet())
+				map.put(Long.parseLong(key), Long.parseLong(map_str.get(key)));
+			OwnMethods.Print(String.format("map size %d", map.size()));
+			
+			inserter = BatchInserters.inserter(new File(db_path),config);
+			for ( int id : spaIDs)
+			{
+				OwnMethods.Print(id);
+				long pos_id = map.get((long)id);
+				inserter.setNodeLabels(pos_id, GraphLabel.GRAPH_1);
+			}
+			inserter.shutdown();
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			if ( inserter != null)
+				inserter.shutdown();
+			System.exit(-1);
+		}
 	}
 	
 	public static void transferGraph()
@@ -71,6 +340,111 @@ public class Graph_Switch {
 				Node osmNode = leafNode.getSingleRelationship(OSMRelation.GEOM, Direction.INCOMING)
 						.getStartNode();
 				
+				//transfer location property
+				if( osmNode.hasProperty(lon_name) == false)
+					throw new Exception(String.format("%s does not has attribute %s", osmNode, lon_name));
+				leafNode.setProperty(lon_name, osmNode.getProperty(lon_name));
+				leafNode.setProperty(lat_name, osmNode.getProperty(lat_name));
+				
+				//transfer node_osm_id property
+				if ( osmNode.hasProperty("node_osm_id") == false)
+					throw new Exception(String.format("%s does not has attribute node_osm_id", osmNode));
+				leafNode.setProperty("node_osm_id", osmNode.getProperty("node_osm_id"));
+				
+				//transfer label
+				Iterable<Label> labels = osmNode.getLabels();
+				boolean has = false;
+				for ( Label label : labels)
+					if ( label.name().equals(GraphLabel.GRAPH_1.name()))
+					{
+						has = true;
+						osmNode.removeLabel(label);
+						break;
+					}
+				if ( has == false)
+					throw new Exception(String.format("%s does not has label %s", osmNode, GraphLabel.GRAPH_1));
+				else
+					leafNode.addLabel(GraphLabel.GRAPH_1);
+				
+				//transfer relationship
+				Iterable<Relationship> rels = osmNode.getRelationships(Direction.INCOMING, GraphRel.GRAPH_LINK);
+				for ( Relationship relationship : rels)
+				{
+					Node startNode = relationship.getStartNode();
+					relationship.delete();
+					startNode.createRelationshipTo(leafNode, GraphRel.GRAPH_LINK);
+				}
+				
+			}
+			tx.success();
+			tx.close();
+			dbservice.shutdown();
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	
+	/**
+	 * generate the map file of graph node id (R-tree version) to neo4j pos
+	 * both spatial and non-spatial vertices are included
+	 */
+	public static void getNodesMapRTree()
+	{
+		try {
+			Map<Object, Object> map = new TreeMap<Object, Object>();
+			
+			GraphDatabaseService dbservice = new GraphDatabaseFactory().newEmbeddedDatabase(new File(db_path));
+			Transaction tx = dbservice.beginTx();
+			
+			int index = 0;
+			
+			//for graph vertices
+			ResourceIterator<Node> nonspatialNodes = dbservice.findNodes(GraphLabel.GRAPH_0);
+			while (nonspatialNodes.hasNext())
+			{
+				OwnMethods.Print(index);	index++;
+				Node node = nonspatialNodes.next();
+				map.put((long)(Integer)node.getProperty("id"), node.getId());
+			}
+			tx.success();
+			tx.close();
+			
+			tx = dbservice.beginTx();
+			Iterable<Node> leafNodes = OSM_Utility.getAllGeometries(dbservice, dataset);
+			for ( Node leafNode : leafNodes)
+			{
+				OwnMethods.Print(index);	index++;
+				map.put(leafNode.getProperty("id"), leafNode.getId());
+			}
+			
+			String map_path = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/node_map_RTree.txt", dataset);
+			OwnMethods.WriteMap(map_path, true, map);
+			
+			tx.success();
+			tx.close();
+			dbservice.shutdown();
+			
+		} catch (Exception e) {
+			e.printStackTrace();System.exit(-1);
+		}
+		
+	}
+	
+	public static void transferProperty()
+	{
+		GraphDatabaseService dbservice = new GraphDatabaseFactory().newEmbeddedDatabase(new File(db_path));
+		try {
+			Transaction tx = dbservice.beginTx();
+			Iterable<Node> leafNodes = OSM_Utility.getAllGeometries(dbservice, dataset);
+			int index = 0;
+			for ( Node leafNode : leafNodes)
+			{
+				OwnMethods.Print(index);	index++;
+				Node osmNode = leafNode.getSingleRelationship(OSMRelation.GEOM, Direction.INCOMING)
+						.getStartNode();
 				
 				//transfer location property
 				if( osmNode.hasProperty(lon_name) == false)
@@ -81,22 +455,7 @@ public class Graph_Switch {
 				//transfer node_osm_id property
 				if ( osmNode.hasProperty("node_osm_id") == false)
 					throw new Exception(String.format("%s does not has attribute node_osm_id", osmNode));
-				leafNode.setProperty("node_osm_id", value);
-				
-				//transfer label
-				Iterable<Label> labels = osmNode.getLabels();
-				boolean has = false;
-				for ( Label label : labels)
-					if ( label.name().equals(GraphLabel.GRAPH_1.name()))
-					{
-						has = true;
-						break;
-					}
-				if ( has == false)
-					throw new Exception(String.format("%s does not has label %s", osmNode, GraphLabel.GRAPH_1));
-				else
-					leafNode.addLabel(GraphLabel.GRAPH_1);
-				
+				leafNode.setProperty("id", osmNode.getProperty("node_osm_id"));
 			}
 			tx.success();
 			tx.close();
