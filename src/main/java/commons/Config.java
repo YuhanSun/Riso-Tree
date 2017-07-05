29c8ac841e71372bce9164849beb7e44949eeaf8
YuhanSun <wdmzssyh@gmail.com>
2017-07-05 14:21:52 -0700

    ignore

--- src/main/java/commons/Config.java
+++ src/main/java/commons/Config.java
@@ -22,16 +22,17 @@ public class Config
 	
 	//attention here, these settings change a lot
 	private String neo4j_version = "neo4j-community-3.1.1";
-	private system operatingSystem = system.Ubuntu;
-	private String dataset = "Gowalla_100";
+	private system operatingSystem = system.Windows;
+	private String dataset = "foursquare";
+	
+	private int MAX_HOPNUM = 1;
+	private int nonspatial_label_count = 1;
 
 	private String Rect_minx_name = "minx";
 	private String Rect_miny_name = "miny";
 	private String Rect_maxx_name = "maxx";
 	private String Rect_maxy_name = "maxy";
 	
-	private int MAX_HOPNUM = 1;
-	private int nonspatial_label_count = 100;
 
 	public String GetServerRoot() {
 		return SERVER_ROOT_URI;
