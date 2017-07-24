3ff42703dcbf8f2fe428b3cfdbea47ef0559ed06
yuhansun <wdmzssyh@gmail.com>
2017-07-23 22:14:52 -0700

    ini

---
+++ src/test/java/graph/Naive_Neo4j_Match_Test.java
@@ -0,0 +1,83 @@
+package graph;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.neo4j.graphdb.Result;
+
+import commons.Config;
+import commons.MyRectangle;
+import commons.OwnMethods;
+import commons.Config.system;
+import commons.Query_Graph;
+import commons.Utility;
+
+public class Naive_Neo4j_Match_Test {
+
+	static Config config = new Config();
+	static String dataset = config.getDatasetName();
+	static String version = config.GetNeo4jVersion();
+	static system systemName = config.getSystemName();
+
+	static String db_path;
+	static String querygraphDir, spaPredicateDir;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		switch (systemName) {
+		case Ubuntu:
+			db_path = String.format("/home/yuhansun/Documents/GeoGraphMatchData/%s_%s/data/databases/graph.db", version, dataset);
+			querygraphDir = String.format("/mnt/hgfs/Google_Drive/Projects/risotree/query/query_graph/%s", dataset);
+			spaPredicateDir = String.format("/mnt/hgfs/Google_Drive/Projects/risotree/query/spa_predicate/%s", dataset);
+			break;
+		case Windows:
+			String dataDirectory = "D:\\Ubuntu_shared\\GeoMinHop\\data";
+			db_path = String.format("%s\\%s\\%s_%s\\data\\databases\\graph.db", dataDirectory, dataset, version, dataset);
+			querygraphDir = String.format("D:\\Ubuntu_shared\\GeoMinHop\\query\\query_graph\\%s", dataset);
+			spaPredicateDir = String.format("D:\\Ubuntu_shared\\GeoMinHop\\query\\spa_predicate\\%s", dataset);
+		default:
+			break;
+		}
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void queryTest() {
+		int nodeCount = 2, query_id = 0;
+		String querygraph_path = String.format("%s/%d.txt", querygraphDir, nodeCount);
+		ArrayList<Query_Graph> queryGraphs = Utility.ReadQueryGraph_Spa(querygraph_path, query_id + 1);
+		Query_Graph query_Graph = queryGraphs.get(query_id);
+		
+		int name_suffix = 75;
+		String queryrect_path = null;
+		switch (systemName) {
+		case Ubuntu:
+			queryrect_path = String.format("%s/queryrect_%d.txt", spaPredicateDir, name_suffix);
+			break;
+		case Windows:
+			queryrect_path = String.format("%s\\queryrect_%d.txt", spaPredicateDir, name_suffix);
+			break;
+		}
+		ArrayList<MyRectangle> queryrect = OwnMethods.ReadQueryRectangle(queryrect_path);
+		MyRectangle rectangle = queryrect.get(0);
+		int j = 0;
+		for (  ; j < query_Graph.graph.size(); j++)
+			if(query_Graph.Has_Spa_Predicate[j])
+				break;
+		query_Graph.spa_predicate[j] = rectangle;
+		
+		Naive_Neo4j_Match naive_Neo4j_Match = new Naive_Neo4j_Match(db_path);
+		Result result = naive_Neo4j_Match.SubgraphMatch_Spa_API(query_Graph, -1);
+		while (result.hasNext())
+			OwnMethods.Print(result.next());
+	}
+
+}
