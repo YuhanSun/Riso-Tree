2b96cbbfdc5de9ccf4e65e8a6012422773ea63df
YuhanSun <wdmzssyh@gmail.com>
2017-09-03 14:06:12 -0700

    add test function

--- src/test/java/graph/RisoTreeTest.java
+++ src/test/java/graph/RisoTreeTest.java
@@ -27,8 +27,10 @@ public class RisoTreeTest {
 	static String db_path, graph_pos_map_path;
 	static String querygraphDir, spaPredicateDir;
 	
-	static int nodeCount = 2, query_id = 0;
-	static int name_suffix = 7549;
+	static int nodeCount = 35, query_id = 0;
+	static int name_suffix = 7;
+//	static int name_suffix = 7549;//0.1
+//	static int name_suffix = 75495;//0.1
 	static String queryrect_path = null, querygraph_path = null;
 	
 	@BeforeClass
@@ -60,35 +62,8 @@ public class RisoTreeTest {
 	public static void tearDownAfterClass() throws Exception {
 	}
 
-//	@Test
-//	public void queryTest() {
-//		ArrayList<Query_Graph> queryGraphs = Utility.ReadQueryGraph_Spa(querygraph_path, query_id + 1);
-//		Query_Graph query_Graph = queryGraphs.get(query_id);
-//		
-//		ArrayList<MyRectangle> queryrect = OwnMethods.ReadQueryRectangle(queryrect_path);
-//		MyRectangle rectangle = queryrect.get(0);
-//		int j = 0;
-//		for (  ; j < query_Graph.graph.size(); j++)
-//			if(query_Graph.Has_Spa_Predicate[j])
-//				break;
-//		query_Graph.spa_predicate[j] = rectangle;
-//		
-//		HashMap<String, String> graph_pos_map = OwnMethods.ReadMap(graph_pos_map_path);
-//		long[] graph_pos_map_list= new long[graph_pos_map.size()];
-//		for ( String key_str : graph_pos_map.keySet())
-//		{
-//			int key = Integer.parseInt(key_str);
-//			int pos_id = Integer.parseInt(graph_pos_map.get(key_str));
-//			graph_pos_map_list[key] = pos_id;
-//		}
-//		RisoTreeQuery risoTreeQuery = new RisoTreeQuery(db_path, dataset, graph_pos_map_list);
-//		risoTreeQuery.Query(query_Graph, -1);
-//		OwnMethods.Print(risoTreeQuery.result_count);
-//		risoTreeQuery.dbservice.shutdown();
-//	}
-	
 	@Test
-	public void queryHMBRTest() {
+	public void queryTest() {
 		ArrayList<Query_Graph> queryGraphs = Utility.ReadQueryGraph_Spa(querygraph_path, query_id + 1);
 		Query_Graph query_Graph = queryGraphs.get(query_id);
 		
@@ -109,9 +84,40 @@ public class RisoTreeTest {
 			graph_pos_map_list[key] = pos_id;
 		}
 		RisoTreeQuery risoTreeQuery = new RisoTreeQuery(db_path, dataset, graph_pos_map_list);
-		risoTreeQuery.QueryHMBR(query_Graph, -1);
-		OwnMethods.Print(risoTreeQuery.result_count);
+		risoTreeQuery.Query(query_Graph, -1);
+		OwnMethods.Print("Result count:" + risoTreeQuery.result_count);
+		OwnMethods.Print("Page access:" + risoTreeQuery.page_hit_count);
+		OwnMethods.Print("get iterator time:" + risoTreeQuery.get_iterator_time);
+		OwnMethods.Print("iterate time:" + risoTreeQuery.iterate_time);
+		
 		risoTreeQuery.dbservice.shutdown();
 	}
+	
+//	@Test
+//	public void queryHMBRTest() {
+//		ArrayList<Query_Graph> queryGraphs = Utility.ReadQueryGraph_Spa(querygraph_path, query_id + 1);
+//		Query_Graph query_Graph = queryGraphs.get(query_id);
+//		
+//		ArrayList<MyRectangle> queryrect = OwnMethods.ReadQueryRectangle(queryrect_path);
+//		MyRectangle rectangle = queryrect.get(0);
+//		int j = 0;
+//		for (  ; j < query_Graph.graph.size(); j++)
+//			if(query_Graph.Has_Spa_Predicate[j])
+//				break;
+//		query_Graph.spa_predicate[j] = rectangle;
+//		
+//		HashMap<String, String> graph_pos_map = OwnMethods.ReadMap(graph_pos_map_path);
+//		long[] graph_pos_map_list= new long[graph_pos_map.size()];
+//		for ( String key_str : graph_pos_map.keySet())
+//		{
+//			int key = Integer.parseInt(key_str);
+//			int pos_id = Integer.parseInt(graph_pos_map.get(key_str));
+//			graph_pos_map_list[key] = pos_id;
+//		}
+//		RisoTreeQuery risoTreeQuery = new RisoTreeQuery(db_path, dataset, graph_pos_map_list);
+//		risoTreeQuery.QueryHMBR(query_Graph, -1);
+//		OwnMethods.Print(risoTreeQuery.result_count);
+//		risoTreeQuery.dbservice.shutdown();
+//	}
 
 }
