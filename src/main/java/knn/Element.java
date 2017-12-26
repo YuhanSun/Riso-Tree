06c59108c5f0ca56696fa5055ec7c028c15990d1
Yuhan Sun <wdmzssyh@gmail.com>
2017-12-25 22:40:00 -0700

    ini

---
+++ src/main/java/knn/Element.java
@@ -0,0 +1,8 @@
+package knn;
+
+import org.neo4j.graphdb.Node;
+
+public class Element {
+	public Node node;
+	public double distance;
+}
---
+++ src/main/java/knn/KNNComparator.java
@@ -0,0 +1,17 @@
+package knn;
+
+import java.util.Comparator;
+
+public class KNNComparator implements Comparator<Element>{
+
+	public int compare(Element o1, Element o2) {
+		// TODO Auto-generated method stub
+		if ( o1.distance < o2.distance)
+			return -1;
+		else if (o1.distance > o2.distance)
+			return 1;
+		else
+			return 0;
+	}
+	
+}
