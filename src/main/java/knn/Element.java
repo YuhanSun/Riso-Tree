9c0ce8a68854a8b40bb822409794584362e97f4f
Yuhan Sun <wdmzssyh@gmail.com>
2017-12-26 12:55:02 -0700

    ini

--- src/main/java/knn/Element.java
+++ src/main/java/knn/Element.java
@@ -5,4 +5,10 @@ import org.neo4j.graphdb.Node;
 public class Element {
 	public Node node;
 	public double distance;
+	
+	public Element(Node node, double distance)
+	{
+		this.node = node;
+		this.distance = distance;
+	}
 }
