1084f42165d70248ccd5dc21411417c099d9efd9
Yuhan Sun <wdmzssyh@gmail.com>
2017-12-26 16:55:27 -0700

    distance test

--- src/test/java/commons/UtilityTest.java
+++ src/test/java/commons/UtilityTest.java
@@ -22,4 +22,13 @@ public class UtilityTest {
 		float lat1 = 10, lat = 12, lon1 = 0, lon2 = 2;
 		OwnMethods.Print("Distance:" + Utility.distFrom(lat1, lon1, lat, lon2));
 	}
+	
+	@Test
+	public void distanceTest()
+	{
+//		double x = 0, y = 0;
+		double x = 1.5, y = 1.5;
+		MyRectangle rectangle = new MyRectangle(1, 1, 2, 2);
+		OwnMethods.Print(Utility.distance(x, y, rectangle));
+	}
 }
