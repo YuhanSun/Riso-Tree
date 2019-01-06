445c062cfb9e345834a3073b8c4b7739d7c92ca3
yuhansun <wdmzssyh@gmail.com>
2019-01-05 19:57:56 -0700

    modify script

--- src/main/script/test.sh
+++ src/main/script/test.sh
@@ -3,11 +3,15 @@ data_dir="${dir}/data"
 
 dataset=""
 dataset_dir="${data_dir}"
+db_path="${dataset_dir}/neo4j-community-3.1.1/data/databases/graph.db_Gleene_1.0"
 graph_path="${dataset_dir}/graph.txt"
-entity_path="${dataset_dir}/graph.txt"
+entity_path="${dataset_dir}/entity.txt"
 label_path="${dataset_dir}/label.txt"
 
 code_dir="${dir}/code"
 jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
 echo "java -Xmx100g -cp ${jar_path} Driver -h"
 java -Xmx100g -cp ${jar_path} Driver -h
+
+echo "java -Xmx100g -cp ${jar_path} Driver -gp ${graph_path} -ep ${entity_path} -lp ${label_path}"
+java -Xmx100g -cp ${jar_path} Driver -gp ${graph_path} -ep ${entity_path} -lp ${label_path}
