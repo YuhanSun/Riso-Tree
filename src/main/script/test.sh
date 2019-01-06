db57650f1d123fc9fe4aa745ec22d9dada671b88
Yuhan Sun <ASUAD\ysun138@en4119507l.cidse.dhcp.asu.edu>
2019-01-05 19:50:06 -0700

    fix the jar path problem

--- src/main/script/test.sh
+++ src/main/script/test.sh
@@ -8,5 +8,6 @@ entity_path="${dataset_dir}/graph.txt"
 label_path="${dataset_dir}/label.txt"
 
 code_dir="${dir}/code"
-jar_path="{code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
-java -Xmx100g -cp jar_path Driver -h
+jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
+echo "java -Xmx100g -cp ${jar_path} Driver -h"
+java -Xmx100g -cp ${jar_path} Driver -h
