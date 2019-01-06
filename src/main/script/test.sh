57ff8659e6fc988ba81ee1e23241d4ed1c0aa742
Yuhan Sun <ASUAD\ysun138@en4119507l.cidse.dhcp.asu.edu>
2019-01-05 20:37:45 -0700

    add -d in script

--- src/main/script/test.sh
+++ src/main/script/test.sh
@@ -1,7 +1,7 @@
 dir="/hdd/code/yuhansun"
 data_dir="${dir}/data"
 
-dataset=""
+dataset="Patents\_100\_random\_20"
 dataset_dir="${data_dir}"
 db_path="${dataset_dir}/neo4j-community-3.1.1/data/databases/graph.db_Gleene_1.0"
 graph_path="${dataset_dir}/graph.txt"
@@ -13,5 +13,5 @@ jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
 echo "java -Xmx100g -cp ${jar_path} Driver -h"
 java -Xmx100g -cp ${jar_path} Driver -h
 
-echo "java -Xmx100g -cp ${jar_path} Driver -f tree -dp ${db_path} -gp ${graph_path} -ep ${entity_path} -lp ${label_path}"
-java -Xmx100g -cp ${jar_path} Driver -f tree -dp ${db_path} -gp ${graph_path} -ep ${entity_path} -lp ${label_path}
+echo "java -Xmx100g -cp ${jar_path} Driver -f tree -dp ${db_path} -d ${dataset} -gp ${graph_path} -ep ${entity_path} -lp ${label_path}"
+java -Xmx100g -cp ${jar_path} Driver -f tree -dp ${db_path} -d ${dataset} -gp ${graph_path} -ep ${entity_path} -lp ${label_path}
