507162d75cba36a186ee7a15e28e52678cab66f0
yuhansun <wdmzssyh@gmail.com>
2019-01-05 18:17:09 -0700

    change the jar_path in test.sh

--- src/main/script/test.sh
+++ src/main/script/test.sh
@@ -1,11 +1,12 @@
-dir="/hdd/code/yuhansun/data"
-code_dir="${dir}/code"
+dir="/hdd/code/yuhansun"
 data_dir="${dir}/data"
 
+dataset=""
 dataset_dir="${data_dir}"
 graph_path="${dataset_dir}/graph.txt"
 entity_path="${dataset_dir}/graph.txt"
 label_path="${dataset_dir}/label.txt"
 
-jar_path="{code_dir}/Riso-Tree-0.0.1-SNAPSHOT.jar"
+code_dir="${dir}/code"
+jar_path="{code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
 java -Xmx100g -cp jar_path Driver -h
