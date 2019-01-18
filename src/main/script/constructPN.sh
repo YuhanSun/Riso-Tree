1eb1643b66b0456e7b0119db3cd50ea3161c6966
yuhansun <wdmzssyh@gmail.com>
2019-01-17 21:49:00 -0700

    add type control

--- src/main/script/constructPN.sh
+++ src/main/script/constructPN.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+tree_type="Gleene_1.0"
+
 # server
 # dir="/hdd/code/yuhansun"
 # dataset="Patents_100_random_20"
@@ -12,7 +14,7 @@ dataset="Yelp"
 data_dir="${dir}/${dataset}"
 code_dir="/Users/zhouyang/Google_Drive/Projects/github_code"
 
-db_path="${data_dir}/neo4j-community-3.1.1/data/databases/graph.db_Gleene_1.0"
+db_path="${data_dir}/neo4j-community-3.1.1/data/databases/graph.db_${tree_type}"
 graph_path="${data_dir}/graph.txt"
 entity_path="${data_dir}/entity.txt"
 label_path="${data_dir}/label.txt"
@@ -22,5 +24,10 @@ jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
 echo "java -Xmx100g -jar ${jar_path} -h"
 java -Xmx100g -jar ${jar_path} -h
 
-echo "java -Xmx100g -jar ${jar_path} -f containID -dp ${db_path} -d ${dataset} -c ${containID_path}"
-java -Xmx100g -jar ${jar_path} -f containID -dp ${db_path} -d ${dataset} -c ${containID_path}
+# echo "java -Xmx100g -jar ${jar_path} -f containID -dp ${db_path} -d ${dataset} -c ${containID_path}"
+# java -Xmx100g -jar ${jar_path} -f containID -dp ${db_path} -d ${dataset} -c ${containID_path}
+
+###### Construct Path Neighbors for leaf nodes ######
+MAX_HOPNUM="2"
+PNPathAndPreffix="${data_dir}/PathNeighbors_${tree_type}"
+java -Xmx100g -jar ${jar_path} -f constructPN -dp ${db_path} -c ${containID_path} -gp ${graph_path} -lp ${label_path} -MAX_HOPNUM ${MAX_HOPNUM} -PNPreffix ${PNPathAndPreffix}
