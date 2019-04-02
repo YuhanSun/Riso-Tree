7e72b4496700cf23055662129de459fa8b80171b
yuhansun <wdmzssyh@gmail.com>
2019-04-02 12:28:38 -0700

    with previous

--- src/test/java/graph/LoadDataNoOSMTest.java
+++ src/test/java/graph/LoadDataNoOSMTest.java
@@ -1,15 +1,12 @@
 package graph;
 
-import static org.junit.Assert.*;
 import java.io.File;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import commons.Config;
 import commons.Config.system;
 import commons.Utility;
@@ -32,43 +29,62 @@ public class LoadDataNoOSMTest {
   static int name_suffix = 75495;// 0.1
   static String queryrect_path = null, querygraph_path = null;
 
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
-    switch (systemName) {
-      case Ubuntu:
-        db_path = String.format(
-            "/home/yuhansun/Documents/GeoGraphMatchData/%s_%s/data/databases/graph.db", version,
-            dataset);
-        graph_pos_map_path =
-            "/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/" + dataset + "/node_map_RTree.txt";
-        querygraphDir =
-            String.format("/mnt/hgfs/Google_Drive/Projects/risotree/query/query_graph/%s", dataset);
-        spaPredicateDir = String
-            .format("/mnt/hgfs/Google_Drive/Projects/risotree/query/spa_predicate/%s", dataset);
-        querygraph_path = String.format("%s/%d.txt", querygraphDir, nodeCount);
-        queryrect_path = String.format("%s/queryrect_%d.txt", spaPredicateDir, name_suffix);
-        break;
-      case Windows:
-        String dataDirectory = "D:\\Ubuntu_shared\\GeoMinHop\\data";
-        db_path = String.format("%s\\%s\\%s_%s\\data\\databases\\graph.db", dataDirectory, dataset,
-            version, dataset);
-        graph_pos_map_path =
-            "D:\\Ubuntu_shared\\GeoMinHop\\data\\" + dataset + "\\node_map_RTree.txt";
-        querygraphDir =
-            String.format("D:\\Google_Drive\\Projects\\risotree\\query\\query_graph\\%s", dataset);
-        spaPredicateDir = String
-            .format("D:\\Google_Drive\\Projects\\risotree\\query\\spa_predicate\\%s", dataset);
-        querygraph_path = String.format("%s\\%d.txt", querygraphDir, nodeCount);
-        queryrect_path = String.format("%s\\queryrect_%d.txt", spaPredicateDir, name_suffix);
-      default:
-        break;
-    }
-    databaseService = new GraphDatabaseFactory().newEmbeddedDatabase(new File(db_path));
+  String homeDir = null;
+  String dataDir = null;
+  String entityPath = null;
+
+  @Before
+  public void setUp() throws Exception {
+    // switch (systemName) {
+    // case Ubuntu:
+    // db_path = String.format(
+    // "/home/yuhansun/Documents/GeoGraphMatchData/%s_%s/data/databases/graph.db", version,
+    // dataset);
+    // graph_pos_map_path =
+    // "/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/" + dataset + "/node_map_RTree.txt";
+    // querygraphDir =
+    // String.format("/mnt/hgfs/Google_Drive/Projects/risotree/query/query_graph/%s", dataset);
+    // spaPredicateDir = String
+    // .format("/mnt/hgfs/Google_Drive/Projects/risotree/query/spa_predicate/%s", dataset);
+    // querygraph_path = String.format("%s/%d.txt", querygraphDir, nodeCount);
+    // queryrect_path = String.format("%s/queryrect_%d.txt", spaPredicateDir, name_suffix);
+    // break;
+    // case Windows:
+    // String dataDirectory = "D:\\Ubuntu_shared\\GeoMinHop\\data";
+    // db_path = String.format("%s\\%s\\%s_%s\\data\\databases\\graph.db", dataDirectory, dataset,
+    // version, dataset);
+    // graph_pos_map_path =
+    // "D:\\Ubuntu_shared\\GeoMinHop\\data\\" + dataset + "\\node_map_RTree.txt";
+    // querygraphDir =
+    // String.format("D:\\Google_Drive\\Projects\\risotree\\query\\query_graph\\%s", dataset);
+    // spaPredicateDir = String
+    // .format("D:\\Google_Drive\\Projects\\risotree\\query\\spa_predicate\\%s", dataset);
+    // querygraph_path = String.format("%s\\%d.txt", querygraphDir, nodeCount);
+    // queryrect_path = String.format("%s\\queryrect_%d.txt", spaPredicateDir, name_suffix);
+    // default:
+    // break;
+    // }
+
+    ClassLoader classLoader = getClass().getClassLoader();
+    File file = new File(classLoader.getResource("").getFile());
+    homeDir = file.getAbsolutePath();
+    dataDir = homeDir + "/data/Yelp";
+    db_path = dataDir + "/test.db";
+    entityPath = dataDir + "/entity.txt";
+
+    // databaseService = new GraphDatabaseFactory().newEmbeddedDatabase(new File(db_path));
   }
 
   @After
   public void tearDown() throws Exception {
-    databaseService.shutdown();
+    // databaseService.shutdown();
+  }
+
+  @Test
+  public void constructRTreeWikidataTest() throws Exception {
+    LoadDataNoOSM loadDataNoOSM = new LoadDataNoOSM(new Config(), true);
+    loadDataNoOSM.loadSpatialEntity(db_path, entityPath);
+    loadDataNoOSM.constructRTreeWikidata(db_path, dataset, entityPath);
   }
 
   @Test
