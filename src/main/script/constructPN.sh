8890dfacbb0f803e1ebf130f2c91c95f7f0d0c05
yuhansun <wdmzssyh@gmail.com>
2019-04-03 14:45:28 -0700

    with previous

--- src/main/script/constructPN.sh
+++ src/main/script/constructPN.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-tree_type="Gleene_1.0"
+# tree_type="Gleene_1.0"
 
 # server
-# dir="/hdd/code/yuhansun"
-# dataset="Patents_100_random_20"
-# data_dir="${dir}/data/${dataset}"
-# code_dir="${dir}/code"
+dir="/hdd/code/yuhansun"
+dataset="Patents_100_random_20"
+data_dir="${dir}/data/${dataset}"
+code_dir="${dir}/code"
 
 # local test setup
-dir="/Users/zhouyang/Google_Drive/Projects/tmp/risotree"
-dataset="Yelp"
-data_dir="${dir}/${dataset}"
-code_dir="/Users/zhouyang/Google_Drive/Projects/github_code"
+# dir="/Users/zhouyang/Google_Drive/Projects/tmp/risotree"
+# dataset="Yelp"
+# data_dir="${dir}/${dataset}"
+# code_dir="/Users/zhouyang/Google_Drive/Projects/github_code"
 
-db_path="${data_dir}/neo4j-community-3.1.1/data/databases/graph.db_${tree_type}"
+db_path="${data_dir}/neo4j-community-3.1.1/data/databases/graph.db"
 graph_path="${data_dir}/graph.txt"
 entity_path="${data_dir}/entity.txt"
 label_path="${data_dir}/label.txt"
@@ -28,10 +28,14 @@ java -Xmx100g -jar ${jar_path} -h
 # java -Xmx100g -jar ${jar_path} -f containID -dp ${db_path} -d ${dataset} -c ${containID_path}
 
 MAX_HOPNUM="2"
-PNPathAndPreffix="${data_dir}/PathNeighbors_${tree_type}"
+PNPathAndPreffix="${data_dir}/PathNeighbors"
+
+# convert single graph to bidirectional
+java -Xmx100g -jar ${jar_path}
+
 
 ###### Construct Path Neighbors for leaf nodes ######
 # java -Xmx100g -jar ${jar_path} -f constructPN -dp ${db_path} -c ${containID_path} -gp ${graph_path} -lp ${label_path} -MAX_HOPNUM ${MAX_HOPNUM} -PNPreffix ${PNPathAndPreffix}
 
 ###### Load PathNeighbor into db ######
-java -Xmx100g -jar ${jar_path} -f loadPN -PNPreffix ${PNPathAndPreffix} -MAX_HOPNUM ${MAX_HOPNUM} -dp ${db_path}
+# java -Xmx100g -jar ${jar_path} -f loadPN -PNPreffix ${PNPathAndPreffix} -MAX_HOPNUM ${MAX_HOPNUM} -dp ${db_path}
