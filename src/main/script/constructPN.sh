59442f4264000e3583bd6bfc0072f35d525f7cf0
yuhansun <wdmzssyh@gmail.com>
2019-05-15 21:15:04 -0700

    run the loading PN

--- src/main/script/constructPN.sh
+++ src/main/script/constructPN.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 ./package.sh
 
-# tree_type="Gleene_1.0"
-
 # server
 dir="/hdd/code/yuhansun"
 dataset="wikidata"
@@ -15,25 +13,27 @@ code_dir="${dir}/code"
 # data_dir="${dir}/${dataset}"
 # code_dir="/Users/zhouyang/Google_Drive/Projects/github_code"
 
-db_path="${data_dir}/neo4j-community-3.4.12/data/databases/graph.db"
 graph_path="${data_dir}/graph.txt"
 entity_path="${data_dir}/entity.txt"
 label_path="${data_dir}/graph_label.txt"
-containID_path="${data_dir}/containID.txt"
 labelStrMapPath="${data_dir}/entity_string_label.txt"
 
 jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
 
-# java -Xmx100g -jar ${jar_path} -h
+split_mode="Gleenes"
+alpha="0.5"
+maxPNSize="100"
 
-# MAX_HOPNUM="2"
-PNPathAndPrefix="${data_dir}/PathNeighbors"
+db_path="${data_dir}/neo4j-community-3.4.12_${split_mode}_${alpha}_${maxPNSize}/data/databases/graph.db"
+containID_path="${data_dir}/containID_${split_mode}_${alpha}_${maxPNSize}.txt"
+PNPathAndPrefix="${data_dir}/PathNeighbors_${split_mode}_${alpha}_${maxPNSize}"
+
 
 # convert single graph to bidirectional
 # java -Xmx100g -jar ${jar_path} -f convertSingleToBidirectinalGraph -dataDir ${data_dir}
 
-# java -Xmx100g -jar ${jar_path} -f wikiGenerateContainSpatialID \
-# 	-dp ${db_path} -d ${dataset} -c ${containID_path}
+java -Xmx100g -jar ${jar_path} -f wikiGenerateContainSpatialID \
+	-dp ${db_path} -d ${dataset} -c ${containID_path}
 
 ###### Wikidata Construct Path Neighbors for leaf nodes ###### (not used)
 # java -Xmx100g -jar ${jar_path} -f wikiConstructPNTime \
@@ -41,25 +41,25 @@ PNPathAndPrefix="${data_dir}/PathNeighbors"
 # 	-lp ${label_path} -MAX_HOPNUM ${MAX_HOPNUM} -PNPrefix ${PNPathAndPrefix}
 
 # 0-hop
-# java -Xmx100g -jar ${jar_path} -f wikiConstructPNTimeSingleHop \
-# 	-dp ${db_path} -c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
-# 	-lp ${label_path} -hop 0 -PNPrefix ${PNPathAndPrefix} -maxPNSize 100
-# java -Xmx100g -jar ${jar_path} -f wikiLoadPN \
-# 	-dp ${db_path} -c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
-# 	-lp ${label_path} -hop 0 -PNPrefix ${PNPathAndPrefix} -maxPNSize 100
-
-# 1-hop
-# java -Xmx100g -jar ${jar_path} -f wikiConstructPNTimeSingleHop \
-# 	-dp ${db_path} -c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
-# 	-lp ${label_path} -hop 1 -PNPrefix ${PNPathAndPrefix} -maxPNSize 100
-# java -Xmx100g -jar ${jar_path} -f wikiLoadPN \
-# 	-dp ${db_path} -c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
-# 	-lp ${label_path} -hop 1 -PNPrefix ${PNPathAndPrefix} -maxPNSize 100
-
-# 2-hop
 java -Xmx100g -jar ${jar_path} -f wikiConstructPNTimeSingleHop \
 	-dp ${db_path} -c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
-	-lp ${label_path} -hop 2 -PNPrefix ${PNPathAndPrefix} -maxPNSize 100
+	-lp ${label_path} -hop 0 -PNPrefix ${PNPathAndPrefix} -maxPNSize ${maxPNSize}
+java -Xmx100g -jar ${jar_path} -f wikiLoadPN \
+	-dp ${db_path} -c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
+	-lp ${label_path} -hop 0 -PNPrefix ${PNPathAndPrefix} -maxPNSize ${maxPNSize}
+
+# 1-hop
+java -Xmx100g -jar ${jar_path} -f wikiConstructPNTimeSingleHop \
+	-dp ${db_path} -c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
+	-lp ${label_path} -hop 1 -PNPrefix ${PNPathAndPrefix} -maxPNSize ${maxPNSize}
+java -Xmx100g -jar ${jar_path} -f wikiLoadPN \
+	-dp ${db_path} -c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
+	-lp ${label_path} -hop 1 -PNPrefix ${PNPathAndPrefix} -maxPNSize ${maxPNSize}
+
+# 2-hop
+# java -Xmx100g -jar ${jar_path} -f wikiConstructPNTimeSingleHop \
+# 	-dp ${db_path} -c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
+# 	-lp ${label_path} -hop 2 -PNPrefix ${PNPathAndPrefix} -maxPNSize 100
 # java -Xmx100g -jar ${jar_path} -f wikiLoadPN \
 # 	-dp ${db_path} -c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
 # 	-lp ${label_path} -hop 2 -PNPrefix ${PNPathAndPrefix}
