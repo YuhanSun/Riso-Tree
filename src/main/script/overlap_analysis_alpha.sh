966d72c55457a566f10b6e1402a7c540d0b0cd93
yuhansun <wdmzssyh@gmail.com>
2019-08-23 23:26:04 -0700

    ignore run

--- src/main/script/overlap_analysis_alpha.sh
+++ src/main/script/overlap_analysis_alpha.sh
@@ -14,8 +14,8 @@ outputPath="${data_dir}/overlap_analysis.csv"
 split_mode="Gleenes"
 maxPNSize="100"
 
-for alpha in 0 0.25 0.5 0.75 1.0
-# for alpha in 0
+# for alpha in 0 0.25 0.5 0.75 1.0
+for alpha in 0
 do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_test"
 	db_path="${data_dir}/neo4j-community-3.4.12_${suffix}/data/databases/graph.db"
