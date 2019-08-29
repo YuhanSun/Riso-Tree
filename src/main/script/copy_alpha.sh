3171d37e67280f94f52da479819ecf1fc8fdb56d
yuhansun <wdmzssyh@gmail.com>
2019-08-29 16:56:06 -0700

    with prev

--- src/main/script/copy_alpha.sh
+++ src/main/script/copy_alpha.sh
@@ -12,6 +12,7 @@ for alpha in 0 0.25 0.5 0.75 1.0
 # for alpha in 0
 do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
+	echo ${suffix}
 
 	source_path="${data_dir}/neo4j-community-3.4.12_node_edges/"
 	target_path="${data_dir}/neo4j-community-3.4.12_${suffix}"
