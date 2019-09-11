f04bb932985e3c4a7abb3197bf3c40cf9c0d6fb4
yuhansun <wdmzssyh@gmail.com>
2019-09-11 15:00:55 -0700

    ignore run

--- src/main/script/maxPNSize_construct_PN_Yelp.sh
+++ src/main/script/maxPNSize_construct_PN_Yelp.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 ./package.sh
 
-dataset="Yelp_100"
-
-# server
-dir="/hdd/code/yuhansun"
-data_dir="${dir}/data/${dataset}"
-code_dir="${dir}/code"
-
-# server setup
-graph_path="${data_dir}/graph.txt"
-entity_path="${data_dir}/entity.txt"
-label_path="${data_dir}/graph_label.txt"
-labelStrMapPath="${data_dir}/entity_string_label.txt"
-spatialNodePNPath="${data_dir}/spatialNodesZeroOneHopPN.txt"
-
-jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
-
-split_mode="Gleenes"
-alpha="1.0"
-# containID_suffix="${split_mode}_${alpha}_new_version"
-containID_path="${data_dir}/containID.txt"
-
-for maxPNSize in 10 20 40 80 160 320 640 1280 2560
+for dataset in "Yelp_100" "Gowalla_100" "Patents_100_random_20"
 do
-	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
-	PNPathAndPrefix="${data_dir}/PathNeighbors_${suffix}"
+	# server
+	dir="/hdd/code/yuhansun"
+	data_dir="${dir}/data/${dataset}"
+	code_dir="${dir}/code"
 
-	# 0-hop
-	java -Xmx100g -jar ${jar_path} -f wikiConstructPNTimeSingleHopNoGraphDb \
-	-c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
-	-lp ${label_path} -hop 0 -PNPrefix ${PNPathAndPrefix} -maxPNSize -1
+	# server setup
+	graph_path="${data_dir}/graph.txt"
+	entity_path="${data_dir}/entity.txt"
+	label_path="${data_dir}/graph_label.txt"
+	labelStrMapPath="${data_dir}/entity_string_label.txt"
+	spatialNodePNPath="${data_dir}/spatialNodesZeroOneHopPN.txt"
 
-	# 1-hop
-	java -Xmx100g -jar ${jar_path} -f wikiConstructPNTimeSingleHopNoGraphDb \
-	-c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
-	-lp ${label_path} -hop 1 -PNPrefix ${PNPathAndPrefix} -maxPNSize ${maxPNSize}
+	jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
 
-	# 2-hop
-	java -Xmx100g -jar ${jar_path} -f wikiConstructPNTimeSingleHopNoGraphDb \
-	-c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
-	-lp ${label_path} -hop 2 -PNPrefix ${PNPathAndPrefix} -maxPNSize ${maxPNSize}
+	split_mode="Gleenes"
+	alpha="1.0"
+	# containID_suffix="${split_mode}_${alpha}_new_version"
+	containID_path="${data_dir}/containID.txt"
 
+	# for maxPNSize in 10 20 40 80 160 320 640 1280 2560
+	for maxPNSize in -1
+	do
+		suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
+		PNPathAndPrefix="${data_dir}/PathNeighbors_${suffix}"
+
+		# 0-hop
+		java -Xmx100g -jar ${jar_path} -f wikiConstructPNTimeSingleHopNoGraphDb \
+		-c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
+		-lp ${label_path} -hop 0 -PNPrefix ${PNPathAndPrefix} -maxPNSize -1
+
+		# 1-hop
+		java -Xmx100g -jar ${jar_path} -f wikiConstructPNTimeSingleHopNoGraphDb \
+		-c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
+		-lp ${label_path} -hop 1 -PNPrefix ${PNPathAndPrefix} -maxPNSize ${maxPNSize}
+
+		# 2-hop
+		java -Xmx100g -jar ${jar_path} -f wikiConstructPNTimeSingleHopNoGraphDb \
+		-c ${containID_path} -gp ${graph_path} -labelStrMapPath ${labelStrMapPath}\
+		-lp ${label_path} -hop 2 -PNPrefix ${PNPathAndPrefix} -maxPNSize ${maxPNSize}
+
+	done
 done
