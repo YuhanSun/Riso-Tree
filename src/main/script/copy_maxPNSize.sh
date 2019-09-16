1edb64f4a7c76a5f2eaaf44eb9c2d3354b6bab6a
yuhansun <wdmzssyh@gmail.com>
2019-09-16 00:40:37 -0700

    ignore run

--- src/main/script/copy_maxPNSize.sh
+++ src/main/script/copy_maxPNSize.sh
@@ -13,7 +13,11 @@ do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
 	echo ${suffix}
 
-	source_path="${data_dir}/neo4j-community-3.4.12_node_edges/"
-	target_path="${data_dir}/neo4j-community-3.4.12_${suffix}"
+	# source_path="${data_dir}/neo4j-community-3.4.12_node_edges/"
+	source_path="$data_dir/neo4j-community-3.4.12_${suffix}"
+	target_path="${data_dir}/backup/neo4j-community-3.4.12_${suffix}"
+
+	echo ${source_path}
+	echo ${target_path}
 	cp -a ${source_path} ${target_path}
 done
