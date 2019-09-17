d8df6714d4d319618fbd6f546a193d8ee54e497b
yuhansun <wdmzssyh@gmail.com>
2019-09-16 20:49:41 -0700

    add the class for tracking variables

--- src/main/java/experiment/ResultRecord.java
+++ src/main/java/experiment/ResultRecord.java
@@ -1,5 +1,9 @@
 package experiment;
 
+import java.util.ArrayList;
+import java.util.List;
+import commons.Util;
+
 public class ResultRecord {
   public long runTime;
   public long pageHit;
@@ -7,5 +11,21 @@ public class ResultRecord {
   public ResultRecord(long runTime, long pageHit) {
     this.runTime = runTime;
     this.pageHit = pageHit;
+  }
+
+  public static long getRunTimeAvg(List<ResultRecord> resultRecords) {
+    List<Long> runTimes = new ArrayList<>(resultRecords.size());
+    for (ResultRecord resultRecord : resultRecords) {
+      runTimes.add(resultRecord.runTime);
+    }
+    return Util.Average(runTimes);
+  }
+
+  public static long getPageHitAvg(List<ResultRecord> resultRecords) {
+    List<Long> pageHits = new ArrayList<>(resultRecords.size());
+    for (ResultRecord resultRecord : resultRecords) {
+      pageHits.add(resultRecord.pageHit);
+    }
+    return Util.Average(pageHits);
   }
 }
