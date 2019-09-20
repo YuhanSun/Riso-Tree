883fa6ab9f8883c2f2751ccda1aa3ff688f3f968
yuhansun <wdmzssyh@gmail.com>
2019-09-20 15:52:11 -0700

    ignore run

--- src/main/script/maxPNSize_query_multiple_queries_single.sh
+++ src/main/script/maxPNSize_query_multiple_queries_single.sh
@@ -3,50 +3,52 @@
 
 dir="/hdd/code/yuhansun"
 
-# for dataset in "Yelp_100" "Gowalla_100" "Patents_100_random_20"
-dataset="Yelp_100"
-
-data_dir="${dir}/data/${dataset}"
-code_dir="${dir}/code"
-result_dir="${dir}/result"
-
-# alpha=1.0
-# split_mode="Gleenes"
-# suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
-db_path="${data_dir}/neo4j-community-3.4.12_Gleenes_1.0_10_new_version/data/databases/graph.db"
-db_path+=",${data_dir}/neo4j-community-3.4.12_Gleenes_1.0_40_new_version/data/databases/graph.db"
-db_path+=",${data_dir}/neo4j-community-3.4.12_Gleenes_1.0_160_new_version/data/databases/graph.db"
-db_path+=",${data_dir}/neo4j-community-3.4.12_Gleenes_1.0_640_new_version/data/databases/graph.db"
-db_path+=",${data_dir}/neo4j-community-3.4.12_Gleenes_1.0_-1_new_version/data/databases/graph.db"
-
-# graph_path="${data_dir}/graph.txt"
-# entity_path="${data_dir}/entity.txt"
-# label_path="${data_dir}/graph_label.txt"
-# labelStrMapPath="${data_dir}/entity_string_label.txt"
-
-query_dir="${dir}/result/query/${dataset}"
-node_count=4
-query_count=100
-# for selectivity in 0.000001 0.00001 0.0001 0.001 0.01
-for selectivity in 0.01 0.001 0.0001 0.00001 0.000001
+for dataset in "Yelp_100" "Gowalla_100" "foursquare_100"
+# dataset="Yelp_100"
 do
-	query_path="${query_dir}/${node_count}_${selectivity}"
 
-	output_path="${result_dir}/Riso-Tree/maxPNSizeRisoTreeQuery"
+	data_dir="${dir}/data/${dataset}"
+	code_dir="${dir}/code"
+	result_dir="${dir}/result"
 
-	jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
-
+	# alpha=1.0
 	# split_mode="Gleenes"
-	# alpha="1.0"
-	# containID_suffix="${split_mode}_${alpha}_new_version"
-	# containID_path="${data_dir}/containID.txt"
+	# suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
+	db_path="${data_dir}/neo4j-community-3.4.12_Gleenes_1.0_10_new_version/data/databases/graph.db"
+	db_path+=",${data_dir}/neo4j-community-3.4.12_Gleenes_1.0_40_new_version/data/databases/graph.db"
+	db_path+=",${data_dir}/neo4j-community-3.4.12_Gleenes_1.0_160_new_version/data/databases/graph.db"
+	db_path+=",${data_dir}/neo4j-community-3.4.12_Gleenes_1.0_640_new_version/data/databases/graph.db"
+	db_path+=",${data_dir}/neo4j-community-3.4.12_Gleenes_1.0_-1_new_version/data/databases/graph.db"
 
-	java -Xmx100g -jar ${jar_path} \
-		-f maxPNSizeRisoTreeQueryMultiple \
-		-dp ${db_path} \
-		-d ${dataset} \
-		-MAX_HOPNUM 2 \
-		-queryPath ${query_path} \
-		-queryCount ${query_count} \
-		-outputPath ${output_path}
+	# graph_path="${data_dir}/graph.txt"
+	# entity_path="${data_dir}/entity.txt"
+	# label_path="${data_dir}/graph_label.txt"
+	# labelStrMapPath="${data_dir}/entity_string_label.txt"
+
+	query_dir="${dir}/result/query/${dataset}"
+	node_count=4
+	query_count=100
+	# for selectivity in 0.000001 0.00001 0.0001 0.001 0.01
+	for selectivity in 0.01 0.001 0.0001 0.00001 0.000001
+	do
+		query_path="${query_dir}/${node_count}_${selectivity}"
+
+		output_path="${result_dir}/Riso-Tree/maxPNSizeRisoTreeQuery"
+
+		jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
+
+		# split_mode="Gleenes"
+		# alpha="1.0"
+		# containID_suffix="${split_mode}_${alpha}_new_version"
+		# containID_path="${data_dir}/containID.txt"
+
+		java -Xmx100g -jar ${jar_path} \
+			-f maxPNSizeRisoTreeQueryMultiple \
+			-dp ${db_path} \
+			-d ${dataset} \
+			-MAX_HOPNUM 2 \
+			-queryPath ${query_path} \
+			-queryCount ${query_count} \
+			-outputPath ${output_path}
+	done
 done
