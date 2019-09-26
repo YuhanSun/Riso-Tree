5c52221b561c5f0bc3f320f94b8571a83d4be745
yuhansun <wdmzssyh@gmail.com>
2019-09-25 22:07:34 -0700

    ignore run

--- src/main/script/overlap_analysis_alpha.sh
+++ src/main/script/overlap_analysis_alpha.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 ./package.sh
 
-dataset="wikidata"
+dataset="Yelp_100"
 
 # server
 dir="/hdd/code/yuhansun"
-data_dir="${dir}/data/wikidata_risotree"
+db_dir="${dir}/data/${dataset}/alpha"
+data_dir="${dir}/data/${dataset}"
 code_dir="${dir}/code"
 
 jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
-outputPath="${data_dir}/overlap_analysis.csv"
+outputPath="${db_dir}/overlap_analysis.csv"
 
 header="db_path\ttotal_overlap\taverage_overlap\n"
 printf ${header} >> ${outputPath}
 
 split_mode="Gleenes"
-maxPNSize="100"
+maxPNSize="-1"
 
 for alpha in 0 0.25 0.5 0.75 1.0
 # for alpha in 0
 do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
-	db_path="${data_dir}/neo4j-community-3.4.12_${suffix}/data/databases/graph.db"
+	db_path="${db_dir}/neo4j-community-3.4.12_${suffix}/data/databases/graph.db"
 
 	java -Xmx100g -jar ${jar_path} \
 	-f overlapAnalysis \
