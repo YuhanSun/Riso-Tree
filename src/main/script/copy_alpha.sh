668b6c0258ab9fa32a048cc86785d5afc7c0c638
yuhansun <wdmzssyh@gmail.com>
2019-09-25 21:22:52 -0700

    add

--- src/main/script/copy_alpha.sh
+++ src/main/script/copy_alpha.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-dataset="wikidata"
+dataset="Yelp_100"
 
 # server
 dir="/hdd/code/yuhansun"
-data_dir="${dir}/data/wikidata_risotree"
+data_dir="${dir}/data/${dataset}"
+db_dir="${dir}/data/${dataset}/alpha"
 
 split_mode="Gleenes"
-maxPNSize="100"
+maxPNSize="-1"
 
 for alpha in 0 0.25 0.5 0.75 1.0
 # for alpha in 0
@@ -14,7 +15,7 @@ do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
 	echo ${suffix}
 
-	source_path="${data_dir}/neo4j-community-3.4.12_node_edges/"
-	target_path="${data_dir}/neo4j-community-3.4.12_${suffix}"
+	source_path="${data_dir}/neo4j-community-3.4.12_node_edge/"
+	target_path="${db_dir}/neo4j-community-3.4.12_${suffix}"
 	cp -a ${source_path} ${target_path}
 done
