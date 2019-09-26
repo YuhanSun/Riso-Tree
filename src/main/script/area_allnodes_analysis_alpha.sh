653c004176767f703b4ea92e5a33b96b542aa499
yuhansun <wdmzssyh@gmail.com>
2019-09-25 22:09:31 -0700

    ignore run

--- src/main/script/area_allnodes_analysis_alpha.sh
+++ src/main/script/area_allnodes_analysis_alpha.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 ./package.sh
 
-dataset="wikidata"
+dataset="Yelp_100"
 
 # server
 dir="/hdd/code/yuhansun"
-data_dir="${dir}/data/wikidata_risotree"
-code_dir="${dir}/code"
+db_dir="${dir}/data/${dataset}/alpha"
+data_dir="${dir}/data/${dataset}"
 
 jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
-outputPath="${data_dir}/area_allnodes_analysis.csv"
+outputPath="${db_dir}/area_allnodes_analysis.csv"
 
 split_mode="Gleenes"
-maxPNSize="100"
+maxPNSize="-1"
 
-# for alpha in 0 0.25 0.5 0.75 1.0
-for alpha in 0.9999 1.0
+for alpha in 0 0.25 0.5 0.75 1.0
+# for alpha in 0.9999 1.0
 do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
-	db_path="${data_dir}/neo4j-community-3.4.12_${suffix}/data/databases/graph.db"
+	db_path="${db_dir}/neo4j-community-3.4.12_${suffix}/data/databases/graph.db"
 
 	java -Xmx100g -jar ${jar_path} \
 	-f treeNodesAvgArea \
