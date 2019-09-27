55ecfce77fb85e113da00ecb6baac20707551f0b
yuhansun <wdmzssyh@gmail.com>
2019-09-26 23:02:49 -0700

    add: experiment addEdge update index

--- src/main/java/graph/MaintenanceUtil.java
+++ src/main/java/graph/MaintenanceUtil.java
@@ -1,86 +1,159 @@
 package graph;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Map;
 import java.util.Queue;
-import java.util.TreeSet;
-import org.neo4j.graphdb.Direction;
+import java.util.Set;
+import org.apache.commons.lang3.StringUtils;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import commons.Config;
-import commons.Labels.RTreeRel;
 import commons.Neo4jGraphUtility;
 import commons.Util;
 
 public class MaintenanceUtil {
 
-  static boolean addEdge(GraphDatabaseService dbservice, Node src, Node trg, int bound) {
-    HashMap<String, HashSet<Node>> pathNeighborsSrc = getPN(dbservice, src, bound - 1);
-    HashMap<String, HashSet<Node>> pathNeighborsTrg = getPN(dbservice, trg, bound - 1);
+  // static boolean addEdge(GraphDatabaseService dbservice, Node src, Node trg, int bound) {
+  // HashMap<String, HashSet<Node>> pathNeighborsSrc = getPN(dbservice, src, bound - 1);
+  // HashMap<String, HashSet<Node>> pathNeighborsTrg = getPN(dbservice, trg, bound - 1);
+  //
+  // for (String propertyName : pathNeighborsSrc.keySet()) {
+  // String[] labelList = propertyName.split("_");
+  // String endLabel = labelList[labelList.length - 1];
+  // // if the end label is 1 which indicates it is spatial
+  // if (endLabel.equals("1")) {
+  // int hopDist = labelList.length - 1;
+  // String prefix = getReversePropertyName(labelList);
+  // HashSet<Node> neighbors = pathNeighborsSrc.get(propertyName);
+  //
+  // List<String> usefulProperties = new ArrayList<>();
+  // for (String trgPropertyName : pathNeighborsTrg.keySet()) {
+  // String[] trgLabelList = trgPropertyName.split("_");
+  // if (trgLabelList.length <= bound - hopDist) {
+  // usefulProperties.add(trgPropertyName);
+  // }
+  // }
+  //
+  // if (usefulProperties.size() > 0) {
+  // HashSet<Node> leafNodes = new HashSet<>();
+  // for (Node node : neighbors) {
+  // Node leafNode = node.getSingleRelationship(RTreeRel.RTREE_REFERENCE, Direction.INCOMING)
+  // .getStartNode();
+  // leafNodes.add(leafNode);
+  // }
+  // for (Node leafNode : leafNodes) {
+  // for (String usefulProperty : usefulProperties) {
+  // String updateProperty = prefix + "_" + usefulProperty;
+  // TreeSet<Integer> updateList = new TreeSet<>();
+  // int[] neighborList = null;
+  // if (leafNode.hasProperty(updateProperty)) {
+  // neighborList = (int[]) leafNode.getProperty(updateProperty);
+  // TreeSet<Integer> addIds = new TreeSet<>();
+  // HashSet<Node> nodes = pathNeighborsTrg.get(usefulProperty);
+  // for (int id : neighborList) {
+  // updateList.add(id);
+  // }
+  // for (Node node : nodes) {
+  // updateList.add((int) node.getProperty("id"));
+  // }
+  // } else {
+  // neighborList = new int[0];
+  // }
+  //
+  // if (updateList.size() > neighborList.length) {
+  // int[] updateArray = new int[updateList.size()];
+  // int i = 0;
+  // for (int element : updateList) {
+  // updateArray[i] = element;
+  // i++;
+  // }
+  // leafNode.setProperty(updateProperty, updateArray);
+  // }
+  // }
+  // }
+  // }
+  //
+  // }
+  // }
+  // return true;
+  // }
 
-    for (String propertyName : pathNeighborsSrc.keySet()) {
-      String[] labelList = propertyName.split("_");
-      String endLabel = labelList[labelList.length - 1];
-      if (endLabel.equals("1")) {
-        int hopDist = labelList.length - 1;
-        String prefix = getReversePropertyName(labelList);
-        HashSet<Node> neighbors = pathNeighborsSrc.get(propertyName);
+  public static String attach(String left, String right) {
+    return left + Config.PNSeparator + right;
+  }
 
-        List<String> usefulProperties = new ArrayList<>();
-        for (String trgPropertyName : pathNeighborsTrg.keySet()) {
-          String[] trgLabelList = trgPropertyName.split("_");
-          if (trgLabelList.length <= bound - hopDist) {
-            usefulProperties.add(trgPropertyName);
-          }
+  /**
+   * Divide a set of nodes based on their labels.
+   *
+   * @param nodes
+   * @return
+   */
+  public static Map<String, Set<Node>> divideByLabel(Set<Node> nodes) {
+    Map<String, Set<Node>> labelNodesMap = new HashMap<>();
+    for (Node node : nodes) {
+      for (Label label : node.getLabels()) {
+        Set<Node> labelNodes = labelNodesMap.get(label.toString());
+        if (labelNodes == null) {
+          labelNodes = new HashSet<>();
+          labelNodesMap.put(label.toString(), labelNodes);
         }
-
-        if (usefulProperties.size() > 0) {
-          HashSet<Node> leafNodes = new HashSet<>();
-          for (Node node : neighbors) {
-            Node leafNode = node.getSingleRelationship(RTreeRel.RTREE_REFERENCE, Direction.INCOMING)
-                .getStartNode();
-            leafNodes.add(leafNode);
-          }
-          for (Node leafNode : leafNodes) {
-            for (String usefulProperty : usefulProperties) {
-              String updateProperty = prefix + "_" + usefulProperty;
-              TreeSet<Integer> updateList = new TreeSet<>();
-              int[] neighborList = null;
-              if (leafNode.hasProperty(updateProperty)) {
-                neighborList = (int[]) leafNode.getProperty(updateProperty);
-                TreeSet<Integer> addIds = new TreeSet<>();
-                HashSet<Node> nodes = pathNeighborsTrg.get(usefulProperty);
-                for (int id : neighborList) {
-                  updateList.add(id);
-                }
-                for (Node node : nodes) {
-                  updateList.add((int) node.getProperty("id"));
-                }
-              } else {
-                neighborList = new int[0];
-              }
-
-              if (updateList.size() > neighborList.length) {
-                int[] updateArray = new int[updateList.size()];
-                int i = 0;
-                for (int element : updateList) {
-                  updateArray[i] = element;
-                  i++;
-                }
-                leafNode.setProperty(updateProperty, updateArray);
-              }
-            }
-          }
-        }
-
+        labelNodes.add(node);
       }
     }
-    return true;
+    return labelNodesMap;
+  }
+
+  /**
+   * Get the PN for a given node.
+   *
+   * @param dbservice
+   * @param source
+   * @param bound
+   * @return
+   */
+  public static Map<String, Set<Node>> getPNGeneral(GraphDatabaseService dbservice, Node source,
+      int bound) {
+    // <PN_propertyName, set_of_nodes>
+    Map<String, Set<Node>> PNMap = new HashMap<>();
+    Queue<String> propertyNames = new LinkedList<>();
+    // Initialize the start state from the source node
+    Map<String, Set<Node>> labelNodesMap = divideByLabel(new HashSet<>(Arrays.asList(source)));
+    for (String labelStr : labelNodesMap.keySet()) {
+      String propertyName = attach(Config.PNPrefix, labelStr);
+      propertyNames.add(propertyName);
+      PNMap.put(propertyName, new HashSet<>(Arrays.asList(source)));
+    }
+    int curCount = propertyNames.size(); // count of PN properties with current hop number
+    for (int i = 1; i <= bound; i++) {
+      int nextCount = 0;
+      for (int j = 0; j < curCount; j++) {
+        String properName = propertyNames.poll();
+        Set<Node> pathNeighbors = PNMap.get(properName);
+        Set<Node> nextNeighbors = getNeighborsInSet(pathNeighbors);
+        Map<String, Set<Node>> nextLabelNodes = divideByLabel(nextNeighbors);
+        for (String labelStr : nextLabelNodes.keySet()) {
+          String nextPropertyName = attach(properName, labelStr);
+          PNMap.put(nextPropertyName, nextLabelNodes.get(labelStr));
+          propertyNames.add(nextPropertyName);
+        }
+        nextCount += nextLabelNodes.size();
+      }
+      curCount = nextCount;
+    }
+    return PNMap;
+  }
+
+  private static Set<Node> getNeighborsInSet(Set<Node> pathNeighbors) {
+    Set<Node> nodes = new HashSet<>();
+    for (Node node : pathNeighbors) {
+      Set<Node> neighbors = Neo4jGraphUtility.getGraphNeighborsNoRTree(node);
+      nodes.addAll(neighbors);
+    }
+    return nodes;
   }
 
   /**
@@ -138,6 +211,12 @@ public class MaintenanceUtil {
     return PNMap;
   }
 
+  /**
+   * Get the id int from "GRAPH_id" format.
+   *
+   * @param node
+   * @return
+   */
   public static int getNodeLabelId(Node node) {
     Iterable<Label> labels = node.getLabels();
     for (Label label : labels) {
@@ -158,6 +237,20 @@ public class MaintenanceUtil {
       prefix += "_" + labelStrings[i];
     }
     return prefix;
+  }
+
+  public static String getReversePnName(String pnName) {
+    String[] strings = StringUtils.split(pnName, Config.PNSeparator);
+    String reverse = strings[0];
+    for (int i = strings.length - 1; i >= 1; i--) {
+      reverse = attach(reverse, strings[i]);
+    }
+    return reverse;
+  }
+
+  public static String concatenatePnNames(String leftPnName, String rightPnName) {
+    String rightLeftOver = rightPnName.replaceFirst(Config.PNPrefix, "");
+    return leftPnName + rightLeftOver;
   }
 
 }
--- src/main/java/graph/RisoTreeMaintenance.java
+++ src/main/java/graph/RisoTreeMaintenance.java
@@ -1,43 +1,214 @@
 package graph;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.TreeSet;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
+import commons.Labels;
 import commons.Neo4jGraphUtility;
+import commons.RTreeUtility;
 import commons.ReadWriteUtil;
+import commons.RisoTreeUtil;
+import commons.Util;
 
 public class RisoTreeMaintenance {
+
   int MAX_HOPNUM;
+  int maxPNSize;
   GraphDatabaseService databaseService;
+  boolean safeNodesUsed;
   /**
    * The vertices that within B-hop from any spatial vertexes. Determine whether the RisoTree update
    * is required.
    */
-  Set<Long> checkList;
+  String safeNodesPath;
+  Set<Long> safeNodes;
 
-  public RisoTreeMaintenance(Set<Long> checkList, int MAX_HOPNUM,
-      GraphDatabaseService databaseService) {
-    this.checkList = checkList;
+
+  public RisoTreeMaintenance(String dbPath, int MAX_HOPNUM, int maxPNSize, Boolean safeNodesUsed,
+      String safeNodesPath) throws Exception {
     this.MAX_HOPNUM = MAX_HOPNUM;
-    this.databaseService = databaseService;
-  }
-
-  public RisoTreeMaintenance(String checkListPath, int MAX_HOPNUM, String dbPath) throws Exception {
-    List<String> arrayList = ReadWriteUtil.readFileAllLines(checkListPath);
-    Set<Long> checkList = new HashSet<>();
-    for (String id : arrayList) {
-      checkList.add(Long.parseLong(id));
+    this.maxPNSize = maxPNSize;
+    this.safeNodesUsed = safeNodesUsed;
+    if (this.safeNodesUsed) {
+      this.safeNodesPath = safeNodesPath;
+      this.safeNodes = readSafeNodes();
     }
-    GraphDatabaseService databaseService = Neo4jGraphUtility.getDatabaseService(dbPath);
-    this.checkList = checkList;
-    this.MAX_HOPNUM = MAX_HOPNUM;
-    this.databaseService = databaseService;
+    this.databaseService = Neo4jGraphUtility.getDatabaseService(dbPath);
   }
 
-  public Relationship addEdge(Node start, Node end) {
-    return null;
+  private Set<Long> readSafeNodes() throws Exception {
+    List<String> arrayList = ReadWriteUtil.readFileAllLines(safeNodesPath);
+    Set<Long> safeNodes = new HashSet<>();
+    for (String id : arrayList) {
+      safeNodes.add(Long.parseLong(id));
+    }
+    return safeNodes;
+  }
+
+  public void addEdge(long src, long trg) {
+    addEdge(databaseService.getNodeById(src), databaseService.getNodeById(trg));
+  }
+
+  public void addEdge(Node src, Node trg) {
+    if (!safeNodes.contains(src.getId()) || !safeNodes.contains(trg.getId())) {
+      addEdgeUpdateCase(src, trg);
+    }
+    src.createRelationshipTo(trg, Labels.GraphRel.GRAPH_INSERT);
+  }
+
+  private void addEdgeUpdateCase(Node src, Node trg) {
+    Map<String, Set<Node>> pathNeighborsSrc =
+        MaintenanceUtil.getPNGeneral(databaseService, src, MAX_HOPNUM - 1);
+    Map<String, Set<Node>> pathNeighborsTrg =
+        MaintenanceUtil.getPNGeneral(databaseService, trg, MAX_HOPNUM - 1);
+
+    // src has spatial pn
+    if (!safeNodes.contains(src.getId())) {
+      Map<String, int[]> pathNeighborsTrgSortedIds = convertToPNSortedIds(pathNeighborsTrg);
+      addEdgeUpdateSingleDirection(pathNeighborsSrc, pathNeighborsTrgSortedIds);
+    }
+    if (!safeNodes.contains(trg.getId())) {
+      Map<String, int[]> pathNeighborsSrcSortedIds = convertToPNSortedIds(pathNeighborsSrc);
+      addEdgeUpdateSingleDirection(pathNeighborsTrg, pathNeighborsSrcSortedIds);
+    }
+  }
+
+  /**
+   * Convert the pns from node format to sorted array format.
+   *
+   * @param pathNeighbors
+   * @return
+   */
+  public static Map<String, int[]> convertToPNSortedIds(Map<String, Set<Node>> pathNeighbors) {
+    Map<String, int[]> pnIds = new HashMap<>();
+    for (String key : pathNeighbors.keySet()) {
+      Set<Node> nodes = pathNeighbors.get(key);
+      TreeSet<Long> sortedIds = new TreeSet<>();
+      for (Node node : nodes) {
+        sortedIds.add(node.getId());
+      }
+      int[] ids = new int[sortedIds.size()];
+      Iterator<Long> iterator = sortedIds.iterator();
+      for (int i = 0; i < ids.length; i++) {
+        long id = iterator.next();
+        if (id > Integer.MAX_VALUE) {
+          throw new RuntimeException(String.format("id is %d exceed integer range!", id));
+        }
+        ids[i] = (int) id;
+      }
+      pnIds.put(key, ids);
+    }
+    return pnIds;
+  }
+
+  /**
+   * Update the pn of the leaf nodes that contain spatial nodes in {@code pathNeighborsSrc}.
+   *
+   * @param pathNeighborsSrc
+   * @param pathNeighborsTrg
+   */
+  private void addEdgeUpdateSingleDirection(Map<String, Set<Node>> pathNeighborsSrc,
+      Map<String, int[]> pathNeighborsTrgSortedIds) {
+    Iterator<Entry<String, Set<Node>>> iterator = pathNeighborsSrc.entrySet().iterator();
+    int minDist = Integer.MAX_VALUE;// min dist from spatial node to src node
+    while (iterator.hasNext()) {
+      Entry<String, Set<Node>> entry = iterator.next();
+      String propertyName = entry.getKey();
+      for (Node node : entry.getValue()) {
+        if (Neo4jGraphUtility.isNodeSpatial(node)) {
+          minDist = Math.min(RisoTreeUtil.getHopNumber(propertyName), minDist);
+          Node leafNode = RTreeUtility.getParentLeafNode(node);
+          updateLeafNodePN(leafNode, propertyName, pathNeighborsTrgSortedIds);
+        }
+      }
+    }
+    if (safeNodesUsed) {
+      updateSafeNodes(pathNeighborsTrgSortedIds, MAX_HOPNUM - minDist);
+    }
+  }
+
+  private void updateSafeNodes(Map<String, int[]> pathNeighborsTrgSortedIds, int updateUpperBound) {
+    Iterator<Entry<String, int[]>> iterator = pathNeighborsTrgSortedIds.entrySet().iterator();
+    while (iterator.hasNext()) {
+      Entry<String, int[]> entry = iterator.next();
+      if (RisoTreeUtil.getHopNumber(entry.getKey()) <= updateUpperBound) {
+        for (int id : entry.getValue()) {
+          safeNodes.add((long) id);
+        }
+      }
+    }
+  }
+
+  /**
+   * If {@code pathNeighborsTrg} contains some nodes that do not exist in {@code leafNode}, expand
+   * {@code leafNode}.
+   *
+   * @param leafNode
+   * @param propertyName
+   * @param pathNeighborsTrg
+   */
+  private void updateLeafNodePN(Node leafNode, String propertyName,
+      Map<String, int[]> pathNeighborsTrg) {
+    String leftPnName = MaintenanceUtil.getReversePnName(propertyName);
+    int length = RisoTreeUtil.getHopNumber(leftPnName);
+    for (String rightPnName : pathNeighborsTrg.keySet()) {
+      int rightLength = RisoTreeUtil.getHopNumber(rightPnName);
+      if (length + rightLength <= MAX_HOPNUM) {
+        int[] nodesAdded = pathNeighborsTrg.get(rightPnName);
+        String pnName = MaintenanceUtil.concatenatePnNames(leftPnName, rightPnName);
+        updateLeafNodeSinglePn(leafNode, pnName, nodesAdded);
+      }
+    }
+  }
+
+  /**
+   * Update the {@code leafNode} of property {@code pnName} with node ids to be added.
+   *
+   * @param leafNode
+   * @param pnName
+   * @param nodesAdded
+   */
+  private void updateLeafNodeSinglePn(Node leafNode, String pnName, int[] nodesAdded) {
+    Set<String> shorterPaths = RisoTreeUtil.formIgnoreSearchSet(pnName);
+    Map<String, Object> properties = leafNode.getAllProperties();
+    Iterator<Map.Entry<String, Object>> iterator = properties.entrySet().iterator();
+    while (iterator.hasNext()) {
+      Map.Entry<String, Object> entry = iterator.next();
+      String key = entry.getKey();
+      if (key.equals(pnName)) { // leafNode has this label path
+        int[] pn = (int[]) entry.getValue();
+        if (pn.length == 0) { // pnName is ignored
+          return;
+        }
+        int[] pnAfterUpdate = Util.sortedArrayMerge(pn, nodesAdded);
+        if (pn.length != pnAfterUpdate.length) {
+          if (pnAfterUpdate.length >= maxPNSize) {
+            pnAfterUpdate = new int[0];
+          }
+          leafNode.setProperty(key, pnAfterUpdate);
+        }
+      } else if (shorterPaths.contains(key)) {
+        int[] pn = (int[]) entry.getValue();
+        if (pn.length == 0) { // pnName is ignored because of shorter paths
+          return;
+        }
+      }
+    }
+  }
+
+  public void deleteEdge(long src, long trg) {
+    deleteEdge(databaseService.getNodeById(src), databaseService.getNodeById(trg));
+  }
+
+  private void deleteEdge(Node nodeById, Node nodeById2) {
+    // TODO Auto-generated method stub
+
   }
 }
