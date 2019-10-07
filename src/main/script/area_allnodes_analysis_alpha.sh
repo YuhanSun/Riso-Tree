363cf6db31af39dd1c3b2365888790f82ef94c03
yuhansun <wdmzssyh@gmail.com>
2019-10-07 15:55:23 -0700

    with prev

--- src/main/script/area_allnodes_analysis_alpha.sh
+++ src/main/script/area_allnodes_analysis_alpha.sh
@@ -15,7 +15,8 @@ outputPath="${db_dir}/area_allnodes_analysis.csv"
 split_mode="Gleenes"
 maxPNSize="-1"
 
-for alpha in 0 0.25 0.5 0.75 1.0
+# for alpha in 0 0.25 0.5 0.75 1.0
+for alpha in 0.55 0.6 0.65 0.7 0.8 0.85 0.9 0.95
 # for alpha in 0.9999 1.0
 do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
