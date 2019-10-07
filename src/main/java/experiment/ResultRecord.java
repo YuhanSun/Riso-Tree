f0019eafd62f8607135b048296e2af454e7a59b7
yuhansun <wdmzssyh@gmail.com>
2019-10-07 02:29:18 -0700

    add: more members to resultrecord

--- src/main/java/experiment/ResultRecord.java
+++ src/main/java/experiment/ResultRecord.java
@@ -8,9 +8,43 @@ public class ResultRecord {
   public long runTime;
   public long pageHit;
 
+  public long range_query_time;
+  public long get_iterator_time;
+  public long iterate_time;
+  public long set_label_time;
+  public long remove_label_time;
+
+  public long result_count;
+  public long overlap_leaf_node_count;
+  public long located_in_count;
+
   public ResultRecord(long runTime, long pageHit) {
     this.runTime = runTime;
     this.pageHit = pageHit;
+  }
+
+  /**
+   * Naive approach
+   *
+   * @param runTime
+   * @param pageHit
+   * @param getIteratorTime
+   * @param iterateTime
+   * @param resultCount
+   */
+  public ResultRecord(long runTime, long pageHit, long getIteratorTime, long iterateTime,
+      long resultCount) {
+    this(runTime, pageHit);
+    this.get_iterator_time = getIteratorTime;
+    this.iterate_time = iterateTime;
+    this.result_count = resultCount;
+  }
+
+  public ResultRecord(long runTime, long pageHit, long getIteratorTime, long iterateTime,
+      long setLabelTime, long removeLabelTime, long resultCount) {
+    this(runTime, pageHit, getIteratorTime, iterateTime, resultCount);
+    this.set_label_time = setLabelTime;
+    this.remove_label_time = removeLabelTime;
   }
 
   public static long getRunTimeAvg(List<ResultRecord> resultRecords) {
