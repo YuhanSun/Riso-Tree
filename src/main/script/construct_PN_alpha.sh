52379e7154aa0f893e0b2b2c1285694e3284c274
yuhansun <wdmzssyh@gmail.com>
2019-10-07 16:38:31 -0700

    with prev

--- src/main/script/construct_PN_alpha.sh
+++ src/main/script/construct_PN_alpha.sh
@@ -33,7 +33,7 @@ jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
 split_mode="Gleenes"
 maxPNSize="-1"
 
-for alpha in 0 0.25 0.5 0.75 1.0
+for alpha in 0 0.25 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0
 # for alpha in 0.9999
 do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
