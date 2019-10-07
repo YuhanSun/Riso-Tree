cb270845ffd377429657141d03d349adf4b51feb
yuhansun <wdmzssyh@gmail.com>
2019-10-07 14:15:09 -0700

    with prev

--- src/main/script/constructTree_alpha.sh
+++ src/main/script/constructTree_alpha.sh
@@ -33,8 +33,9 @@ jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
 split_mode="Gleenes"
 maxPNSize="-1"
 
-for alpha in 0 0.25 0.5 0.75 1.0
+# for alpha in 0 0.25 0.5 0.75 1.0
 # for alpha in 0
+for alpha in 0.55 0.6 0.65 0.7 0.8 0.85 0.9 0.95
 do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
 
