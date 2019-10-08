7843bb0d42c3017ca0530ccf7e7b3f452b035806
yuhansun <wdmzssyh@gmail.com>
2019-10-07 17:19:21 -0700

    with prev

--- src/main/script/analysis_alpha.sh
+++ src/main/script/analysis_alpha.sh
@@ -36,7 +36,8 @@ printf ${header} >> ${outputPath}
 split_mode="Gleenes"
 maxPNSize="-1"
 
-for alpha in 0 0.25 0.5 0.75 1.0
+# for alpha in 0 0.25 0.5 0.75 1.0
+for alpha in 0.55 0.6 0.65 0.7 0.8 0.85 0.9 0.95
 # for alpha in 0
 do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
@@ -58,7 +59,8 @@ done
 printf "\n" >> ${outputPath}
 printf ${header} >> ${outputPath}
 
-for alpha in 0 0.25 0.5 0.75 1.0
+# for alpha in 0 0.25 0.5 0.75 1.0
+for alpha in 0.55 0.6 0.65 0.7 0.8 0.85 0.9 0.95
 do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
 	# suffix="${split_mode}_${alpha}_${maxPNSize}_test"
@@ -73,7 +75,8 @@ done
 printf "\n" >> ${outputPath}
 printf ${header} >> ${outputPath}
 
-for alpha in 0 0.25 0.5 0.75 1.0
+# for alpha in 0 0.25 0.5 0.75 1.0
+for alpha in 0.55 0.6 0.65 0.7 0.8 0.85 0.9 0.95
 do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
 	# suffix="${split_mode}_${alpha}_${maxPNSize}_test"
