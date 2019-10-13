e62627bba74c59701e26944884bfad70ca7cef0d
yuhansun <wdmzssyh@gmail.com>
2019-10-12 19:24:39 -0700

    with prev

--- src/main/script/alpha_construct_all.sh
+++ src/main/script/alpha_construct_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./package.sh
+jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
 
 dataset="Yelp_100"
 
@@ -25,7 +26,7 @@ do
 	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
 	echo ${suffix}
 
-	source_path="${data_dir}/neo4j-community-3.4.12_node_edge/"
+	source_path="${data_dir}/neo4j-community-3.4.12_node_edges/"
 	target_path="${db_dir}/neo4j-community-3.4.12_${suffix}"
 	rm -r ${target_path}
 	cp -a ${source_path} ${target_path}
