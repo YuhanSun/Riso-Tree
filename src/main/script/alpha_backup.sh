e84e245fada70c60f31efe36881342ff90c39bbc
yuhansun <wdmzssyh@gmail.com>
2019-10-13 17:07:22 -0700

    add alpha-related .sh

---
+++ src/main/script/alpha_backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+dataset="Yelp_100"
+
+# server
+dir="/hdd/code/yuhansun"
+data_dir="${dir}/data/${dataset}"
+db_dir="${dir}/data/${dataset}/alpha"
+backup_dir="${db_dir}/backup"
+
+split_mode="Gleenes"
+maxPNSize="-1"
+
+# for alpha in 0 0.25 0.5 0.75 1.0
+# for alpha in 0
+for alpha in 0 0.9 0.99 0.999 0.9999 1.0
+do
+	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
+	echo ${suffix}
+
+	source_path="${db_dir}/neo4j-community-3.4.12_${suffix}/"
+	target_path="${backup_dir}/neo4j-community-3.4.12_${suffix}"
+	rm -r ${target_path}
+	cp -a ${source_path} ${target_path}
+done
---
+++ src/main/script/alpha_load_pn.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+./package.sh
+
+dataset="Yelp_100"
+
+# server
+dir="/hdd/code/yuhansun"
+data_dir="${dir}/data/${dataset}"
+db_dir="data_dir/alpha"
+code_dir="${dir}/code"
+jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
+
+maxPNSize=-1
+split_mode="Gleenes"
+
+for alpha in 0 0.9 0.99 0.999 0.9999 1.0
+do
+	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
+	containId_suffix="${split_mode}_${alpha}_new_version"
+	containID_path="${data_dir}/containID_${containId_suffix}.txt"
+	db_path="${db_dir}/neo4j-community-3.4.12_${suffix}/data/databases/graph.db"
+	PNPathAndPrefix="${data_dir}/PathNeighbors_${suffix}"
+	java -Xmx100g -jar ${jar_path} \
+		-f wikiLoadAllHopPN \
+		-PNPrefix ${PNPathAndPrefix} \
+		-hopListStr 0,1,2 \
+		-dp ${db_path} \
+		-c ${containID_path}		
+done
