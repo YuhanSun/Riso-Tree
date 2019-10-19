243fd699cbcaa1d6b21bc48051c05015d2f3acd8
yuhansun <wdmzssyh@gmail.com>
2019-10-18 22:48:49 -0700

    fix path in selectivity_query

--- src/main/script/selectivity_query.sh
+++ src/main/script/selectivity_query.sh
@@ -8,7 +8,7 @@ MAX_HOPNUM=2
 password="syhSYH.19910205"
 
 data_dir="${dir}/data/${dataset}"
-db_dir="${data_dir}/alpha"
+db_dir="${data_dir}/selectivity"
 code_dir="${dir}/code"
 result_dir="${dir}/result/Riso-Tree/selectivity"
 query_dir="${dir}/result/query/${dataset}"
@@ -21,8 +21,6 @@ maxPNSize="-1"
 query_path=""
 for selectivity in 0.0001 0.001 0.01 0.1
 do
-	suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
-
 	if [ -z "$query_path" ];	then
 		query_path="${query_dir}/${query_size}_${selectivity}"
 	else
@@ -34,6 +32,8 @@ output_path="${result_dir}"
 log_path="${result_dir}/${dataset}_log.txt"
 
 jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
+suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
+db_path="${db_dir}/neo4j-community-3.4.12_${suffix}/data/databases/graph.db"
 
 java -Xmx100g -jar ${jar_path} \
 	-f selectivityExperiment \
