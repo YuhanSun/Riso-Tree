2b2f364a35e1675c243a8d49440f7732c16a220a
yuhansun <wdmzssyh@gmail.com>
2019-10-23 11:47:47 -0700

    add  comment and map function for rectangle

--- src/main/java/commons/LocationMap.java
+++ src/main/java/commons/LocationMap.java
@@ -1,5 +1,12 @@
 package commons;
 
+/**
+ * Map the point in {@code fromExtend} to {@code toExtend}. The mapping follows a linear scaling way
+ * from the (0,0) origin.
+ *
+ * @author yuhan sun
+ *
+ */
 public class LocationMap {
   MyRectangle fromExtend;
   MyRectangle toExtend;
@@ -17,5 +24,13 @@ public class LocationMap {
     double x = toExtend.min_x + (fromPoint.x - fromExtend.min_x) * widthRatio;
     double y = toExtend.min_y + (fromPoint.y - fromExtend.min_y) * heightRatio;
     return new MyPoint(x, y);
+  }
+
+  public MyRectangle map(MyRectangle rectangle) {
+    MyPoint fromLeftBottom = new MyPoint(rectangle.min_x, rectangle.min_y);
+    MyPoint fromRightTop = new MyPoint(rectangle.max_x, rectangle.max_y);
+    MyPoint toLeftBottom = map(fromLeftBottom);
+    MyPoint toRightTop = map(fromRightTop);
+    return new MyRectangle(toLeftBottom.x, toLeftBottom.y, toRightTop.x, toRightTop.y);
   }
 }
