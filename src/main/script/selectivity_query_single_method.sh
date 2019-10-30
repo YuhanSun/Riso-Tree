5df66ec418c018ea57731d2eca1edd8e6c35305a
yuhansun <wdmzssyh@gmail.com>
2019-10-29 18:51:34 -0700

    ignore run

--- src/main/script/selectivity_query_single_method.sh
+++ src/main/script/selectivity_query_single_method.sh
@@ -25,7 +25,7 @@ query_dir="${dir}/result/query/${dataset}"
 mkdir -p $result_dir
 
 query_size=2
-query_count=2
+query_count=5
 
 split_mode="Gleenes"
 maxPNSize="-1"
@@ -48,39 +48,12 @@ suffix="${split_mode}_${alpha}_${maxPNSize}_new_version"
 db_path="${db_dir}/neo4j-community-3.4.12_${suffix}/data/databases/graph.db"
 
 ##### SPATIAL_FIRST ######
+log_path="${result_dir}/${dataset}_spatialfirst_log.txt"
+run_spatial=1
+if [ $run_spatial == 1 ];	then
 # java -Xmx100g -jar ${jar_path} \
 # 	-f selectivityExperimentSingleMethod \
 # 	-method "SPATIAL_FIRST"	\
-# 	-dp ${db_path} \
-# 	-d ${dataset} \
-# 	-MAX_HOPNUM ${MAX_HOPNUM} \
-# 	-queryPath ${query_path} \
-# 	-queryCount ${query_count} \
-# 	-password ${password}	\
-# 	-clearCache "true"	\
-# 	-clearCacheMethod "DOUBLE" \
-# 	-outputPath ${output_path} \
-# 	>> ${log_path}
-
-# java -Xmx100g -jar ${jar_path} \
-# 	-f selectivityExperimentSingleMethod \
-# 	-method "SPATIAL_FIRST"	\
-# 	-dp ${db_path} \
-# 	-d ${dataset} \
-# 	-MAX_HOPNUM ${MAX_HOPNUM} \
-# 	-queryPath ${query_path} \
-# 	-queryCount ${query_count} \
-# 	-password ${password}	\
-# 	-clearCache "false"	\
-# 	-clearCacheMethod "NULL" \
-# 	-outputPath ${output_path} \
-# 	>> ${log_path}
-
-##### RISOTREE ######
-log_path="${result_dir}/${dataset}_risotree_log.txt"
-# java -Xmx100g -jar ${jar_path} \
-# 	-f selectivityExperimentSingleMethod \
-# 	-method "RISOTREE"	\
 # 	-dp ${db_path} \
 # 	-d ${dataset} \
 # 	-MAX_HOPNUM ${MAX_HOPNUM} \
@@ -94,7 +67,7 @@ log_path="${result_dir}/${dataset}_risotree_log.txt"
 
 java -Xmx100g -jar ${jar_path} \
 	-f selectivityExperimentSingleMethod \
-	-method "RISOTREE"	\
+	-method "SPATIAL_FIRST"	\
 	-dp ${db_path} \
 	-d ${dataset} \
 	-MAX_HOPNUM ${MAX_HOPNUM} \
@@ -105,3 +78,37 @@ java -Xmx100g -jar ${jar_path} \
 	-clearCacheMethod "NULL" \
 	-outputPath ${output_path} \
 	>> ${log_path}
+fi
+
+##### RISOTREE ######
+log_path="${result_dir}/${dataset}_risotree_log.txt"
+run_risotree=1
+if [ $run_spatial == 1 ];	then
+	# java -Xmx100g -jar ${jar_path} \
+	# 	-f selectivityExperimentSingleMethod \
+	# 	-method "RISOTREE"	\
+	# 	-dp ${db_path} \
+	# 	-d ${dataset} \
+	# 	-MAX_HOPNUM ${MAX_HOPNUM} \
+	# 	-queryPath ${query_path} \
+	# 	-queryCount ${query_count} \
+	# 	-password ${password}	\
+	# 	-clearCache "true"	\
+	# 	-clearCacheMethod "DOUBLE" \
+	# 	-outputPath ${output_path} \
+	# 	>> ${log_path}
+
+	java -Xmx100g -jar ${jar_path} \
+		-f selectivityExperimentSingleMethod \
+		-method "RISOTREE"	\
+		-dp ${db_path} \
+		-d ${dataset} \
+		-MAX_HOPNUM ${MAX_HOPNUM} \
+		-queryPath ${query_path} \
+		-queryCount ${query_count} \
+		-password ${password}	\
+		-clearCache "false"	\
+		-clearCacheMethod "NULL" \
+		-outputPath ${output_path} \
+		>> ${log_path}
+fi
