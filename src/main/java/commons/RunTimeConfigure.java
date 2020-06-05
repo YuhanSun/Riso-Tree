1974c38ad85221ebffaafbec3821a6df24d1d13e
yuhansun <YuhanSun@asu.edu>
2020-06-04 23:25:55 -0700

    add more configurations

--- src/main/java/commons/RunTimeConfigure.java
+++ src/main/java/commons/RunTimeConfigure.java
@@ -3,8 +3,12 @@ package commons;
 import commons.Enums.ClearCacheMethod;
 
 public class RunTimeConfigure {
-  public static final String dataset = "foursquare_100";
-  public static final String suffix = "Gleenes_1.0_-1_new_version";
+  public static final String dataset = "wikidata";
+  public static final String suffix = "node_edges";
+  // public static final String suffix = "Gleenes_1.0_-1_new_version";
+  // public static final String suffix = "Gleenes_0.9_-1_new_version";
+  // public static final String suffix = "Gleenes_1.0_640_new_version";
+
   // String dbPath =
   // "D:\\Project_Data\\wikidata-20180308-truthy-BETA.nt\\neo4j-community-3.4.12_risotree_test\\data\\databases\\graph.db";
   // String dbPath =
@@ -15,9 +19,9 @@ public class RunTimeConfigure {
 
   public static final String queryDir =
       "D:\\Google_Drive\\Projects\\risotree\\cypher_query\\" + dataset;
-  public static final String queryPath = queryDir + "\\5_0.000001";
+  public static final String queryPath = queryDir + "\\2_0.000001";
   // String queryPath = queryDir + "\\4_0.0001";
-  public static final int queryCount = 9;
+  public static final int queryCount = 2;
 
   public static final boolean clearCache = false;
   public static final Enums.ClearCacheMethod clearMethod = Enums.ClearCacheMethod.DOUBLE;
