ada77fc2e470a912845284118bcdf85a33c8ee7e
yuhansun <wdmzssyh@gmail.com>
2020-07-11 17:56:54 -0700

    fix error CypherUtilTest.java

--- src/test/java/CypherMiddleWare/CypherUtilTest.java
+++ src/test/java/CypherMiddleWare/CypherUtilTest.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import commons.Util;
 import cypher.middleware.CypherUtil;
 
-class CypherUtilTest {
+public class CypherUtilTest {
 
   @Test
-  void removeWhereTest() {
+  public void removeWhereTest() {
     String query =
         "match (a0:`hill`),(a1:`member state of the United Nations`),(a0)--(a1) where -104.442650 <= a0.lon <= -104.361190 and 27.044770 <= a0.lat <= 27.126230 return id(a0),id(a1)";
     Util.println(CypherUtil.removeWhere(query));
